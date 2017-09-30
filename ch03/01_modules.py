1bac03a2eecde7bc6a0652367cbae6088b33ab57
Max Lapan <max.lapan@gmail.com>
2017-09-30 08:39:47 +0200

    Ch3 samples

---
+++ ch03/01_modules.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+class OurModule(nn.Module):
+    def __init__(self, num_inputs, num_classes, dropout_prob=0.3):
+        super(OurModule, self).__init__()
+        self.pipe = nn.Sequential(
+            nn.Linear(num_inputs, 5),
+            nn.ReLU(),
+            nn.Linear(5, 20),
+            nn.ReLU(),
+            nn.Linear(20, num_classes),
+            nn.Dropout(p=dropout_prob),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.pipe(x)
+
+if __name__ == "__main__":
+    net = OurModule(num_inputs=2, num_classes=3)
+    print(net)
+    v = Variable(torch.FloatTensor([[2, 3]]))
+    out = net(v)
+    print(out)
