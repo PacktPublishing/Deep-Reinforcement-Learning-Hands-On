a89d606e2a36a85099474040ada9843a45f173b0
Max Lapan <max.lapan@gmail.com>
2017-10-06 09:46:37 +0200

    Nonslippery version

---
+++ ch04/04_frozenlake_nonslippery.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+import random
+import gym
+import gym.spaces
+import gym.wrappers
+import gym.envs.toy_text.frozen_lake
+from collections import namedtuple
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+
+
+HIDDEN_SIZE = 128
+BATCH_SIZE = 100
+PERCENTILE = 30
+GAMMA = 0.9
+
+
+class DiscreteOneHotWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(DiscreteOneHotWrapper, self).__init__(env)
+        assert isinstance(env.observation_space, gym.spaces.Discrete)
+        self.observation_space = gym.spaces.Box(0.0, 1.0, (env.observation_space.n, ))
+
+    def _observation(self, observation):
+        res = np.copy(self.observation_space.low)
+        res[observation] = 1.0
+        return res
+
+
+class Net(nn.Module):
+    def __init__(self, obs_size, hidden_size, n_actions):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+Episode = namedtuple('Episode', field_names=['reward', 'steps'])
+EpisodeStep = namedtuple('EpisodeStep', field_names=['observation', 'action'])
+
+
+def iterate_batches(env, net, batch_size):
+    batch = []
+    episode_reward = 0.0
+    episode_steps = []
+    obs = env.reset()
+    sm = nn.Softmax()
+    while True:
+        obs_v = Variable(torch.FloatTensor([obs]))
+        act_probs_v = sm(net(obs_v))
+        act_probs = act_probs_v.data.numpy()[0]
+        action = np.random.choice(len(act_probs), p=act_probs)
+        next_obs, reward, is_done, _ = env.step(action)
+        episode_reward += reward
+        episode_steps.append(EpisodeStep(observation=obs, action=action))
+        if is_done:
+            batch.append(Episode(reward=episode_reward, steps=episode_steps))
+            episode_reward = 0.0
+            episode_steps = []
+            next_obs = env.reset()
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        obs = next_obs
+
+
+def filter_batch(batch, percentile):
+    disc_rewards = list(map(lambda s: s.reward * (GAMMA ** len(s.steps)), batch))
+    reward_bound = np.percentile(disc_rewards, percentile)
+
+    train_obs = []
+    train_act = []
+    elite_batch = []
+    for example, discounted_reward in zip(batch, disc_rewards):
+        if discounted_reward > reward_bound:
+            train_obs.extend(map(lambda step: step.observation, example.steps))
+            train_act.extend(map(lambda step: step.action, example.steps))
+            elite_batch.append(example)
+
+    train_obs_v = Variable(torch.FloatTensor(train_obs))
+    train_act_v = Variable(torch.LongTensor(train_act))
+    return elite_batch, train_obs_v, train_act_v, reward_bound
+
+
+if __name__ == "__main__":
+    random.seed(12345)
+    env = gym.envs.toy_text.frozen_lake.FrozenLakeEnv(is_slippery=False)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=100)
+    env = DiscreteOneHotWrapper(env)
+    # env = gym.wrappers.Monitor(env, directory="mon", force=True)
+    obs_size = env.observation_space.shape[0]
+    n_actions = env.action_space.n
+
+    net = Net(obs_size, HIDDEN_SIZE, n_actions)
+    objective = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.001)
+    writer = SummaryWriter(comment="-frozenlake-nonslippery")
+
+    full_batch = []
+    for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
+        reward_mean = float(np.mean(list(map(lambda s: s.reward, batch))))
+        full_batch, obs_v, acts_v, reward_bound = filter_batch(full_batch + batch, PERCENTILE)
+        if not full_batch:
+            continue
+        full_batch = full_batch[-500:]
+
+        optimizer.zero_grad()
+        action_scores_v = net(obs_v)
+        loss_v = objective(action_scores_v, acts_v)
+        loss_v.backward()
+        optimizer.step()
+        print("%d: loss=%.3f, reward_mean=%.3f, reward_bound=%.3f, batch=%d" % (
+            iter_no, loss_v.data[0], reward_mean, reward_bound, len(full_batch)))
+        writer.add_scalar("loss", loss_v.data[0], iter_no)
+        writer.add_scalar("reward_mean", reward_mean, iter_no)
+        writer.add_scalar("reward_bound", reward_bound, iter_no)
+        if reward_mean > 0.8:
+            print("Solved!")
+            break
+    writer.close()
