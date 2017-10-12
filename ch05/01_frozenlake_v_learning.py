561fbf2fb61fb3fe3eb769629d5aaeace4477c12
Max Lapan <max.lapan@gmail.com>
2017-10-12 14:53:45 +0200

    Value iteration for FrozenLake

---
+++ ch05/01_frozenlake_v_learning.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+import gym
+import collections
+from tensorboardX import SummaryWriter
+
+GAMMA = 0.9
+TEST_EPISODES = 100
+
+RewardKey = collections.namedtuple('RewardKey', field_names=['src_state', 'action', 'tgt_state'])
+TransitKey = collections.namedtuple('TransitKey', field_names=['src_state', 'action'])
+
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+        self.state = self.env.reset()
+        self.rewards = collections.defaultdict(float)
+        self.transits = collections.defaultdict(collections.Counter)
+
+    def play_n_random_steps(self, count):
+        for _ in range(count):
+            action = self.env.action_space.sample()
+            new_state, reward, is_done, _ = self.env.step(action)
+            self.rewards[RewardKey(self.state, action, new_state)] = reward
+            self.transits[TransitKey(self.state, action)][new_state] += 1
+            self.state = self.env.reset() if is_done else new_state
+
+    def select_action(self, state, values):
+        best_action, best_value = None, None
+        for action in range(self.env.action_space.n):
+            target_counts = self.transits[TransitKey(state, action)]
+            total = sum(target_counts.values())
+            action_value = 0.0
+            for tgt_state, count in target_counts.items():
+                reward = self.rewards[RewardKey(state, action, tgt_state)]
+                action_value += (count / total) * (reward + GAMMA * values[tgt_state])
+            if best_value is None or best_value < action_value:
+                best_value = action_value
+                best_action = action
+        if best_action is not None:
+            return best_action
+        return self.env.action_space.sample()
+
+    def play_episode(self, values):
+        total_reward = 0.0
+        state = self.env.reset()
+        while True:
+            action = self.select_action(state, values)
+            state, reward, is_done, _ = self.env.step(action)
+            total_reward += reward
+            if is_done:
+                break
+        return total_reward
+
+
+def value_iteration(values, agent):
+    assert isinstance(agent, Agent)
+    for state in range(agent.env.observation_space.n):
+        state_values = []
+        for action in range(agent.env.action_space.n):
+            action_value = 0.0
+            target_counts = agent.transits[TransitKey(state, action)]
+            total = sum(target_counts.values())
+            for tgt_state, count in target_counts.items():
+                reward = agent.rewards[RewardKey(state, action, tgt_state)]
+                action_value += (count / total) * (reward + GAMMA * values[tgt_state])
+            state_values.append(action_value)
+        values[state] = max(state_values)
+
+
+if __name__ == "__main__":
+    env = gym.make("FrozenLake-v0")
+    agent = Agent(env)
+    writer = SummaryWriter()
+    values = collections.defaultdict(float)
+
+    iter_no = 0
+    best_reward = 0.0
+    while True:
+        iter_no += 1
+        agent.play_n_random_steps(100)
+        value_iteration(values, agent)
+
+        reward = 0.0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(values)
+        reward /= TEST_EPISODES
+        writer.add_scalar("reward", reward, iter_no)
+        if reward > best_reward:
+            print("Best reward updated %.3f -> %.3f" % (best_reward, reward))
+            best_reward = reward
+        if reward > 0.78:
+            print("Solved in %d iterations!" % iter_no)
+            break
+    writer.close()
