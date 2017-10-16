d3085bb6a661c00d9db6611f62c2ab00d0ab5259
Max Lapan <max.lapan@gmail.com>
2017-10-16 16:39:21 +0200

    dqn

--- ch06/01_dqn_pong.py
+++ ch06/01_dqn_pong.py
@@ -5,7 +5,8 @@ import numpy as np
 from scipy.misc import imresize
 
 import torch
-import torchvision.utils as tv_utils
+import torch.nn as nn
+from torch.autograd import Variable
 
 
 class ImageWrapper(gym.ObservationWrapper):
@@ -31,7 +32,6 @@ class BufferWrapper(gym.ObservationWrapper):
                                                 old_space.high.repeat(n_steps, axis=0))
 
     def _reset(self):
-        print("Reset")
         self.buffer = self.observation_space.low.copy()
         return self._observation(self.env.reset())
 
@@ -41,8 +41,37 @@ class BufferWrapper(gym.ObservationWrapper):
         return self.buffer
 
 
+class DQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(DQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 16, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2),
+            nn.ReLU(),
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
+
+
 if __name__ == "__main__":
     env = BufferWrapper(ImageWrapper(gym.make("Pong-v4")), n_steps=4)
-    print(env.observation_space)
-    print(env.action_space)
+    net = DQN(env.observation_space.shape, env.action_space.n)
+    print(net)
+    out = net(Variable(torch.FloatTensor([env.reset()])))
+    print(out.size())
     pass
