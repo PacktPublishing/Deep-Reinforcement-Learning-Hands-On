e941f3cd3d11dc881cb32042c8e61f5118849477
Max Lapan <max.lapan@gmail.com>
2017-10-28 21:47:38 +0200

    Ind noise

--- ch07/04_dqn_noisy_net.py
+++ ch07/04_dqn_noisy_net.py
@@ -73,8 +73,8 @@ if __name__ == "__main__":
     frame_idx = 0
     writer = SummaryWriter(comment="-pong-noisy-net")
     def writer_func(val, name):
-        writer.add_scalar(val, name, frame_idx)
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n, noisy_net=True, writer=writer)
+        writer.add_scalar(name, val.data.cpu().numpy(), frame_idx)
+    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n, noisy_net=True, writer=writer_func)
     if args.cuda:
         net.cuda()
 
