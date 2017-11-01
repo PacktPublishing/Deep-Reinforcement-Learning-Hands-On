b1e07310e37b85cb25523c6c638e70380131fdb0
Max Lapan <max.lapan@gmail.com>
2017-11-01 19:10:25 +0100

    Test of distributions

---
+++ ch07/distr_test.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from lib import tools
+
+Vmax = 10
+Vmin = -10
+N_ATOMS = 51
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
+
+
+if __name__ == "__main__":
+    np.random.seed(123)
+    atoms = np.arange(Vmin, Vmax+DELTA_Z, DELTA_Z)
+
+    # normal distribution
+    data = np.random.normal(size=1000, scale=3)
+    hist = np.histogram(data, bins=np.arange(Vmin - DELTA_Z/2, Vmax + DELTA_Z*3/2, DELTA_Z))
+    tools.save_distr(atoms, hist[0], "normal-01")
+
+    src_hist = hist[0]
+    proj_hist = tools.distr_projection(np.array([src_hist]), np.array([2], dtype=np.float32),
+                                       Vmin, Vmax, N_ATOMS, gamma=0.9)
+    tools.save_distr(atoms, proj_hist[0], "normal-02")
+
+    # single peak distribution
+    src_hist = np.zeros(shape=(1, N_ATOMS), dtype=np.float32)
+    src_hist[0, N_ATOMS//2+1] = 1.0
+    tools.save_distr(atoms, src_hist[0], "peak-01")
+    proj_hist = tools.distr_projection(src_hist, np.array([2], dtype=np.float32),
+                                       Vmin, Vmax, N_ATOMS, gamma=0.9)
+    tools.save_distr(atoms, proj_hist[0], "peak-02")
+    pass
--- ch07/lib/tools.py
+++ ch07/lib/tools.py
@@ -3,14 +3,32 @@ import matplotlib as mpl
 mpl.use("Agg")
 import matplotlib.pyplot as plt
 
-Vmax = 10
-Vmin = -10
-N_ATOMS = 51
-DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
+# Vmax = 10
+# Vmin = -10
+# N_ATOMS = 51
+# DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
 
 
-def save_distr(vec, name):
+def save_distr(atoms, vec, name):
     plt.cla()
-    p = np.arange(Vmin, Vmax+DELTA_Z, DELTA_Z)
-    plt.bar(p, vec)
+#    p = np.arange(Vmin, Vmax+DELTA_Z, DELTA_Z)
+    plt.bar(atoms, vec, width=0.5)
     plt.savefig(name + ".png")
+
+
+def distr_projection(next_distr, rewards, Vmin, Vmax, n_atoms, gamma):
+    """
+    Perform distribution projection aka Catergorical Algorithm from the
+    "A Distributional Perspective on RL" paper
+    """
+    batch_size = len(rewards)
+    proj_distr = np.zeros((batch_size, n_atoms), dtype=np.float32)
+    delta_z = (Vmax - Vmin) / (n_atoms - 1)
+    for atom in range(n_atoms):
+        tz_j = rewards + (Vmin + atom * delta_z) * gamma
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j)
+        u = np.ceil(b_j)
+        proj_distr[:, atom] += next_distr[:, atom] * (u - b_j)
+        proj_distr[:, atom] += next_distr[:, atom] * (b_j - l)
+    return proj_distr
