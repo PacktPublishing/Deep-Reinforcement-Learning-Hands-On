77cf8ce8b967f42aa0ead9931584a0ed68cbb034
Max Lapan <max.lapan@gmail.com>
2017-11-01 15:00:58 +0100

    First version of categorical

--- ch07/07_dqn_categorical.py
+++ ch07/07_dqn_categorical.py
@@ -8,12 +8,11 @@ import argparse
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
 from tensorboardX import SummaryWriter
-
-from lib import dqn_model
 
 PONG_MODE = True
 
@@ -22,7 +21,7 @@ if PONG_MODE:
     MEAN_REWARD_BOUND = 19.5
     RUN_NAME = "pong"
     REPLAY_SIZE = 10000
-    REPLAY_START_SIZE = 10000
+    REPLAY_START_SIZE = 100 # TODO: return back
     EPSILON_DECAY_LAST_FRAME = 10 ** 5
 else:
     DEFAULT_ENV_NAME = "BreakoutNoFrameskip-v4"
@@ -43,6 +42,7 @@ EPSILON_FINAL = 0.02
 Vmax = 10
 Vmin = -10
 N_ATOMS = 51
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
 
 
 class CategoricalDQN(nn.Module):
@@ -65,6 +65,10 @@ class CategoricalDQN(nn.Module):
             nn.Linear(512, n_actions * N_ATOMS)
         )
 
+        support_t = torch.arange(Vmin, Vmax, DELTA_Z)
+        self.register_buffer("supports", Variable(support_t, volatile=True))
+        self.softmax = nn.Softmax()
+
     def _get_conv_out(self, shape):
         o = self.conv(Variable(torch.zeros(1, *shape)))
         return int(np.prod(o.size()))
@@ -76,23 +80,18 @@ class CategoricalDQN(nn.Module):
         fc_out = self.fc(conv_out)
         return fc_out.view(batch_size, -1, N_ATOMS)
 
-
-class CategoricalQValues(nn.Module):
-    """
-    Wrapper to calculate QValues from categorical distribution
-    """
-    def __init__(self, cat_net):
-        super(CategoricalQValues, self).__init__()
-        self.cat_net = cat_net
-        self.register_buffer("supports", Variable(torch.arange(Vmin, Vmax, (Vmax-Vmin)/(N_ATOMS-1)), volatile=True))
-        self.softmax = nn.Softmax()
-
-    def forward(self, x):
-        cat_out = self.cat_net(x)
-        probs = self.softmax(cat_out.view(-1, N_ATOMS)).view(cat_out.size())
+    def both(self, x):
+        cat_out = self(x)
+        probs = self.apply_softmax(cat_out)
         weights = probs * self.supports
         res = weights.sum(dim=2)
-        return res
+        return cat_out, res
+
+    def qvals(self, x):
+        return self.both(x)[1]
+
+    def apply_softmax(self, t):
+        return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
 
 
 def unpack_batch(batch):
@@ -113,26 +112,51 @@ def unpack_batch(batch):
 
 def calc_loss(batch, net, tgt_net, cuda=False):
     states, actions, rewards, dones, next_states = unpack_batch(batch)
+    batch_size = len(batch)
 
     states_v = Variable(torch.from_numpy(states))
-    next_states_v = Variable(torch.from_numpy(next_states), volatile=True)
     actions_v = Variable(torch.from_numpy(actions))
-    rewards_v = Variable(torch.from_numpy(rewards))
-    done_mask = torch.ByteTensor(dones)
+    next_states_v = Variable(torch.from_numpy(next_states), volatile=True)
     if cuda:
         states_v = states_v.cuda()
-        next_states_v = next_states_v.cuda()
         actions_v = actions_v.cuda()
-        rewards_v = rewards_v.cuda()
-        done_mask = done_mask.cuda()
+        next_states_v = next_states_v.cuda()
 
-    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
-    next_state_values = tgt_net(next_states_v).max(1)[0]
-    next_state_values[done_mask] = 0.0
-    next_state_values.volatile = False
+    # next state distribution
+    next_distr_v, next_qvals_v = tgt_net.both(next_states_v)
+    next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
+    next_distr = tgt_net.apply_softmax(next_distr_v).data.cpu().numpy()
 
-    expected_state_action_values = next_state_values * GAMMA + rewards_v
-    return nn.MSELoss()(state_action_values, expected_state_action_values)
+    # in paper: p (distribution for the next best action)
+    next_best_distr = next_distr[range(batch_size), next_actions]
+
+    # for samples at the end of episode, next distribution will have 1 probability at 0 score
+    dones = dones.astype(np.bool)
+    next_best_distr[dones] = 0.0
+    next_best_distr[dones, N_ATOMS//2] = 1.0
+
+    # in paper: m (projected distribution)
+    proj_distr = np.zeros((batch_size, N_ATOMS), dtype=np.float32)
+
+    for atom in range(N_ATOMS):
+        tz_j = rewards + (Vmin + atom * DELTA_Z) * GAMMA
+        b_j = (tz_j - Vmin) / DELTA_Z
+        l = np.floor(b_j)
+        u = np.ceil(b_j)
+        l_mask = l >= 0
+        u_mask = u < N_ATOMS
+        proj_distr[l_mask, atom] += next_best_distr[l_mask, atom] * (u[l_mask] - b_j[l_mask])
+        proj_distr[u_mask, atom] += next_best_distr[u_mask, atom] * (b_j[u_mask] - l[u_mask])
+
+    # calculate net output
+    distr_v = net(states_v)
+    state_action_values = distr_v[range(batch_size), actions_v.data]
+    state_log_sm_v = F.log_softmax(state_action_values)
+    proj_distr_v = Variable(torch.from_numpy(proj_distr))
+    if cuda:
+        proj_distr_v = proj_distr_v.cuda()
+    loss_v = -state_log_sm_v * proj_distr_v
+    return loss_v.sum(dim=1).mean()
 
 
 if __name__ == "__main__":
@@ -143,16 +167,14 @@ if __name__ == "__main__":
     env = gym.make(DEFAULT_ENV_NAME)
     env = ptan.common.wrappers.wrap_dqn(env)
 
-    writer = SummaryWriter(comment="-" + RUN_NAME + "-categorical")
+   writer = SummaryWriter(comment="-" + RUN_NAME + "-categorical")
     net = CategoricalDQN(env.observation_space.shape, env.action_space.n)
-    qvals_net = CategoricalQValues(net)
     if args.cuda:
         net.cuda()
-        qvals_net.cuda()
 
     tgt_net = ptan.agent.TargetNet(net)
     epsilon_greedy_selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=1.0)
-    agent = ptan.agent.DQNAgent(qvals_net, epsilon_greedy_selector, cuda=args.cuda)
+    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), epsilon_greedy_selector, cuda=args.cuda)
 
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA, steps_count=1)
     buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
@@ -199,4 +221,4 @@ if __name__ == "__main__":
 
         if frame_idx % SYNC_TARGET_FRAMES == 0:
             tgt_net.sync()
-    writer.close()
+   writer.close()
