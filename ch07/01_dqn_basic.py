b1906f25d14917a4379f026a91b88b04a129c48c
Max Lapan <max.lapan@gmail.com>
2017-11-02 11:38:37 +0100

    Refactor reward reporting and epsilon decay

--- ch07/01_dqn_basic.py
+++ ch07/01_dqn_basic.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-import sys
 import gym
 import ptan
-import time
-import numpy as np
 import argparse
 
 import torch.optim as optim
@@ -28,53 +25,35 @@ if __name__ == "__main__":
         net.cuda()
 
     tgt_net = ptan.agent.TargetNet(net)
-    epsilon_greedy_selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
-    agent = ptan.agent.DQNAgent(net, epsilon_greedy_selector, cuda=args.cuda)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(net, selector, cuda=args.cuda)
 
     exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
     buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
     optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
 
     frame_idx = 0
-    ts_frame = 0
-    ts = time.time()
 
-    total_rewards = []
-    while True:
-        frame_idx += 1
-        buffer.populate(1)
-        epsilon_greedy_selector.epsilon = max(params['epsilon_final'],
-                                              params['epsilon_start'] - frame_idx / params['epsilon_frames'])
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+            epsilon_tracker.frame(frame_idx)
 
-        new_rewards = exp_source.pop_total_rewards()
-        if new_rewards:
-            total_rewards.extend(new_rewards)
-            speed = (frame_idx - ts_frame) / (time.time() - ts)
-            ts_frame = frame_idx
-            ts = time.time()
-            mean_reward = np.mean(total_rewards[-100:])
-            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
-                frame_idx, len(total_rewards), mean_reward, epsilon_greedy_selector.epsilon,
-                speed
-            ))
-            sys.stdout.flush()
-            writer.add_scalar("epsilon", epsilon_greedy_selector.epsilon, frame_idx)
-            writer.add_scalar("speed", speed, frame_idx)
-            writer.add_scalar("reward_100", mean_reward, frame_idx)
-            writer.add_scalar("reward", new_rewards[0], frame_idx)
-            if mean_reward > params['stop_reward']:
-                print("Solved in %d frames!" % frame_idx)
-                break
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
+                    break
 
-        if len(buffer) < params['replay_initial']:
-            continue
+            if len(buffer) < params['replay_initial']:
+                continue
 
-        optimizer.zero_grad()
-        batch = buffer.sample(params['batch_size'])
-        loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
-        loss_v.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            batch = buffer.sample(params['batch_size'])
+            loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
+            loss_v.backward()
+            optimizer.step()
 
-        if frame_idx % params['target_net_sync'] == 0:
-            tgt_net.sync()
-    writer.close()
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
--- ch07/lib/common.py
+++ ch07/lib/common.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import numpy as np
 import torch
 import torch.nn as nn
@@ -75,6 +77,54 @@ def calc_loss_dqn(batch, net, tgt_net, gamma, cuda=False):
     expected_state_action_values = next_state_values * gamma + rewards_v
     return nn.MSELoss()(state_action_values, expected_state_action_values)
 
+
+class RewardTracker:
+    def __init__(self, writer, stop_reward):
+        self.writer = writer
+        self.stop_reward = stop_reward
+
+    def __enter__(self):
+        self.ts = time.time()
+        self.ts_frame = 0
+        self.total_rewards = []
+        return self
+
+    def __exit__(self, *args):
+        self.writer.close()
+
+    def reward(self, reward, frame, epsilon=None):
+        self.total_rewards.append(reward)
+        speed = (frame - self.ts_frame) / (time.time() - self.ts)
+        self.ts_frame = frame
+        self.ts = time.time()
+        mean_reward = np.mean(self.total_rewards[-100:])
+        epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon
+        print("%d: done %d games, mean reward %.3f, speed %.2f f/s%s" % (
+            frame, len(self.total_rewards), mean_reward, speed, epsilon_str
+        ))
+        sys.stdout.flush()
+        if epsilon is not None:
+            self.writer.add_scalar("epsilon", epsilon, frame)
+        self.writer.add_scalar("speed", speed, frame)
+        self.writer.add_scalar("reward_100", mean_reward, frame)
+        self.writer.add_scalar("reward", reward, frame)
+        if mean_reward > self.stop_reward:
+            print("Solved in %d frames!" % frame)
+            return True
+        return False
+
+
+class EpsilonTracker:
+    def __init__(self, epsilon_greedy_selector, params):
+        self.epsilon_greedy_selector = epsilon_greedy_selector
+        self.epsilon_start = params['epsilon_start']
+        self.epsilon_final = params['epsilon_final']
+        self.epsilon_frames = params['epsilon_frames']
+        self.frame(0)
+
+    def frame(self, frame):
+        self.epsilon_greedy_selector.epsilon = \
+            max(self.epsilon_final, self.epsilon_start - frame / self.epsilon_frames)
 
 
 def distr_projection(next_distr, rewards, dones, Vmin, Vmax, n_atoms, gamma):
