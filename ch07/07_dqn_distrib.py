e0518cd2535c3782768a54640fa7745dd08e3959
Max Lapan <max.lapan@gmail.com>
2017-11-02 12:07:44 +0100

    Refactor done

--- ch07/07_dqn_distrib.py
+++ ch07/07_dqn_distrib.py
@@ -16,33 +16,6 @@ from tensorboardX import SummaryWriter
 
 from lib import common
 
-PONG_MODE = True
-
-if PONG_MODE:
-    DEFAULT_ENV_NAME = "PongNoFrameskip-v4"
-    MEAN_REWARD_BOUND = 19.5
-    RUN_NAME = "pong"
-    REPLAY_SIZE = 10000
-    SYNC_TARGET_FRAMES = 1000
-    REPLAY_START_SIZE = 10000
-    EPSILON_DECAY_LAST_FRAME = 10 ** 5
-    EPSILON_FINAL = 0.02
-    LEARNING_RATE = 0.0001
-else:
-    DEFAULT_ENV_NAME = "BreakoutNoFrameskip-v4"
-    MEAN_REWARD_BOUND = 500
-    RUN_NAME = "breakout"
-    REPLAY_SIZE = 1000000
-    REPLAY_START_SIZE = 50000
-    SYNC_TARGET_FRAMES = 10000
-    EPSILON_DECAY_LAST_FRAME = 10**6
-    EPSILON_FINAL = 0.1
-    LEARNING_RATE = 0.00025
-
-GAMMA = 0.99
-BATCH_SIZE = 32
-EPSILON_START = 1.0
-
 Vmax = 10
 Vmin = -10
 N_ATOMS = 51
@@ -97,7 +70,7 @@ class DistributionalDQN(nn.Module):
         return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
 
 
-def calc_loss(batch, net, tgt_net, cuda=False):
+def calc_loss(batch, net, tgt_net, gamma, cuda=False):
     states, actions, rewards, dones, next_states = common.unpack_batch(batch)
     batch_size = len(batch)
 
@@ -114,14 +87,11 @@ def calc_loss(batch, net, tgt_net, cuda=False):
     next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
     next_distr = tgt_net.apply_softmax(next_distr_v).data.cpu().numpy()
 
-    # in paper: p (distribution for the next best action)
     next_best_distr = next_distr[range(batch_size), next_actions]
-
-    # for samples at the end of episode, next distribution will have 1 probability at 0 score
     dones = dones.astype(np.bool)
 
     # project our distribution using Bellman update
-    proj_distr = common.distr_projection(next_best_distr, rewards, dones, Vmin, Vmax, N_ATOMS, GAMMA)
+    proj_distr = common.distr_projection(next_best_distr, rewards, dones, Vmin, Vmax, N_ATOMS, gamma)
 
     # calculate net output
     distr_v = net(states_v)
@@ -135,65 +105,49 @@ def calc_loss(batch, net, tgt_net, cuda=False):
 
 
 if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
     args = parser.parse_args()
 
-    env = gym.make(DEFAULT_ENV_NAME)
+    env = gym.make(params['env_name'])
     env = ptan.common.wrappers.wrap_dqn(env)
 
-    writer = SummaryWriter(comment="-" + RUN_NAME + "-distrib")
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-distrib")
     net = DistributionalDQN(env.observation_space.shape, env.action_space.n)
     if args.cuda:
         net.cuda()
 
     tgt_net = ptan.agent.TargetNet(net)
-    epsilon_greedy_selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=1.0)
-    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), epsilon_greedy_selector, cuda=args.cuda)
+    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params['epsilon_start'])
+    epsilon_tracker = common.EpsilonTracker(selector, params)
+    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), selector, cuda=args.cuda)
 
-    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA, steps_count=1)
-    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
-    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=params['replay_size'])
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
 
     frame_idx = 0
-    ts_frame = 0
-    ts = time.time()
 
-    total_rewards = []
-    while True:
-        frame_idx += 1
-        buffer.populate(1)
-        epsilon_greedy_selector.epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+            epsilon_tracker.frame(frame_idx)
 
-        new_rewards = exp_source.pop_total_rewards()
-        if new_rewards:
-            total_rewards.extend(new_rewards)
-            speed = (frame_idx - ts_frame) / (time.time() - ts)
-            ts_frame = frame_idx
-            ts = time.time()
-            mean_reward = np.mean(total_rewards[-100:])
-            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
-                frame_idx, len(total_rewards), mean_reward, epsilon_greedy_selector.epsilon,
-                speed
-            ))
-            sys.stdout.flush()
-            writer.add_scalar("epsilon", epsilon_greedy_selector.epsilon, frame_idx)
-            writer.add_scalar("speed", speed, frame_idx)
-            writer.add_scalar("reward_100", mean_reward, frame_idx)
-            writer.add_scalar("reward", new_rewards[0], frame_idx)
-            if mean_reward > MEAN_REWARD_BOUND:
-                print("Solved in %d frames!" % frame_idx)
-                break
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
+                    break
 
-        if len(buffer) < REPLAY_START_SIZE:
-            continue
+            if len(buffer) < params['replay_initial']:
+                continue
 
-        optimizer.zero_grad()
-        batch = buffer.sample(BATCH_SIZE)
-        loss_v = calc_loss(batch, net, tgt_net.target_model, cuda=args.cuda)
-        loss_v.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            batch = buffer.sample(params['batch_size'])
+            loss_v = calc_loss(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
+            loss_v.backward()
+            optimizer.step()
 
-        if frame_idx % SYNC_TARGET_FRAMES == 0:
-            tgt_net.sync()
-    writer.close()
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
