2274a76ad8c91d11a82d92fca7badb1092316ca1
Max Lapan <max.lapan@gmail.com>
2017-11-02 16:37:30 +0100

    Return back old ver

---
+++ ch07/07_dqn_categorical.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python3
+import sys
+import gym
+import ptan
+import time
+import numpy as np
+import argparse
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from tensorboardX import SummaryWriter
+
+from lib import tools
+
+PONG_MODE = True
+
+if PONG_MODE:
+    DEFAULT_ENV_NAME = "PongNoFrameskip-v4"
+    MEAN_REWARD_BOUND = 19.5
+    RUN_NAME = "pong"
+    REPLAY_SIZE = 10000
+    REPLAY_START_SIZE = 10000
+    EPSILON_DECAY_LAST_FRAME = 10 ** 5
+else:
+    DEFAULT_ENV_NAME = "BreakoutNoFrameskip-v4"
+    MEAN_REWARD_BOUND = 500
+    RUN_NAME = "breakout"
+    REPLAY_SIZE = 100000
+    REPLAY_START_SIZE = 50000
+    EPSILON_DECAY_LAST_FRAME = 10**6
+
+GAMMA = 0.99
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-4
+SYNC_TARGET_FRAMES = 1000
+
+EPSILON_START = 1.0
+EPSILON_FINAL = 0.02
+
+Vmax = 10
+Vmin = -10
+N_ATOMS = 51
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
+
+
+class CategoricalDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(CategoricalDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions * N_ATOMS)
+        )
+
+        self.register_buffer("supports", torch.arange(Vmin, Vmax, DELTA_Z))
+        self.softmax = nn.Softmax()
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(batch_size, -1)
+        fc_out = self.fc(conv_out)
+        return fc_out.view(batch_size, -1, N_ATOMS)
+
+    def both(self, x):
+        cat_out = self(x)
+        probs = self.apply_softmax(cat_out)
+        weights = probs * Variable(self.supports, volatile=True)
+        res = weights.sum(dim=2)
+        return cat_out, res
+
+    def qvals(self, x):
+        return self.both(x)[1]
+
+    def apply_softmax(self, t):
+        return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
+
+
+def unpack_batch(batch):
+    states, actions, rewards, dones, last_states = [], [], [], [], []
+    for exp in batch:
+        state = np.array(exp.state, copy=False)
+        states.append(state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        dones.append(exp.last_state is None)
+        if exp.last_state is None:
+            last_states.append(state)       # the result will be masked anyway
+        else:
+            last_states.append(np.array(exp.last_state, copy=False))
+    return np.array(states, copy=False), np.array(actions), np.array(rewards, dtype=np.float32), \
+           np.array(dones, dtype=np.uint8), np.array(last_states, copy=False)
+
+
+def calc_loss(batch, net, tgt_net, cuda=False):
+    states, actions, rewards, dones, next_states = unpack_batch(batch)
+    batch_size = len(batch)
+
+    states_v = Variable(torch.from_numpy(states))
+    actions_v = Variable(torch.from_numpy(actions))
+    next_states_v = Variable(torch.from_numpy(next_states), volatile=True)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_v = actions_v.cuda()
+        next_states_v = next_states_v.cuda()
+
+    # next state distribution
+    next_distr_v, next_qvals_v = tgt_net.both(next_states_v)
+    next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
+    next_distr = tgt_net.apply_softmax(next_distr_v).data.cpu().numpy()
+
+    # in paper: p (distribution for the next best action)
+    next_best_distr = next_distr[range(batch_size), next_actions]
+
+    # for samples at the end of episode, next distribution will have 1 probability at 0 score
+    dones = dones.astype(np.bool)
+
+    # project our distribution using Bellman update
+    proj_distr = tools.distr_projection(next_best_distr, rewards, dones, Vmin, Vmax, N_ATOMS, GAMMA)
+
+    # calculate net output
+    distr_v = net(states_v)
+    state_action_values = distr_v[range(batch_size), actions_v.data]
+    state_log_sm_v = F.log_softmax(state_action_values)
+    proj_distr_v = Variable(torch.from_numpy(proj_distr))
+    if cuda:
+        proj_distr_v = proj_distr_v.cuda()
+    loss_v = -state_log_sm_v * proj_distr_v
+    return loss_v.sum(dim=1).mean()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+
+    env = gym.make(DEFAULT_ENV_NAME)
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + RUN_NAME + "-categorical")
+    net = CategoricalDQN(env.observation_space.shape, env.action_space.n)
+    if args.cuda:
+        net.cuda()
+
+    tgt_net = ptan.agent.TargetNet(net)
+    epsilon_greedy_selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=1.0)
+    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), epsilon_greedy_selector, cuda=args.cuda)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA, steps_count=1)
+    buffer = ptan.experience.ExperienceReplayBuffer(exp_source, buffer_size=REPLAY_SIZE)
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    frame_idx = 0
+    ts_frame = 0
+    ts = time.time()
+
+    total_rewards = []
+    while True:
+        frame_idx += 1
+        buffer.populate(1)
+        epsilon_greedy_selector.epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+
+        new_rewards = exp_source.pop_total_rewards()
+        if new_rewards:
+            total_rewards.extend(new_rewards)
+            speed = (frame_idx - ts_frame) / (time.time() - ts)
+            ts_frame = frame_idx
+            ts = time.time()
+            mean_reward = np.mean(total_rewards[-100:])
+            print("%d: done %d games, mean reward %.3f, eps %.2f, speed %.2f f/s" % (
+                frame_idx, len(total_rewards), mean_reward, epsilon_greedy_selector.epsilon,
+                speed
+            ))
+            sys.stdout.flush()
+            writer.add_scalar("epsilon", epsilon_greedy_selector.epsilon, frame_idx)
+            writer.add_scalar("speed", speed, frame_idx)
+            writer.add_scalar("reward_100", mean_reward, frame_idx)
+            writer.add_scalar("reward", new_rewards[0], frame_idx)
+            if mean_reward > MEAN_REWARD_BOUND:
+                print("Solved in %d frames!" % frame_idx)
+                break
+
+        if len(buffer) < REPLAY_START_SIZE:
+            continue
+
+        optimizer.zero_grad()
+        batch = buffer.sample(BATCH_SIZE)
+        loss_v = calc_loss(batch, net, tgt_net.target_model, cuda=args.cuda)
+        loss_v.backward()
+        optimizer.step()
+
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            tgt_net.sync()
+    writer.close()
