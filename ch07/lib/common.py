ea5d21ee6ef7c335cb8b0fc99611f522eb70a94f
Max Lapan <max.lapan@gmail.com>
2017-11-03 18:12:25 +0100

    Try smaller lr

--- ch07/lib/common.py
+++ ch07/lib/common.py
@@ -16,8 +16,8 @@ HYPERPARAMS = {
         'target_net_sync':  1000,
         'epsilon_frames':   10**5,
         'epsilon_start':    1.0,
-        'epsilon_final':    0.1,
-        'learning_rate':    0.0001,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.00005,
         'gamma':            0.99,
         'batch_size':       32
     },
