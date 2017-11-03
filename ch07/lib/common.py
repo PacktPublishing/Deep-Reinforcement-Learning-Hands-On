b45885952dcbc00773cf47003a3678295f786b27
Max Lapan <max.lapan@gmail.com>
2017-11-03 16:55:02 +0100

    Higher final epsilon

--- ch07/lib/common.py
+++ ch07/lib/common.py
@@ -16,7 +16,7 @@ HYPERPARAMS = {
         'target_net_sync':  1000,
         'epsilon_frames':   10**5,
         'epsilon_start':    1.0,
-        'epsilon_final':    0.02,
+        'epsilon_final':    0.1,
         'learning_rate':    0.0001,
         'gamma':            0.99,
         'batch_size':       32
