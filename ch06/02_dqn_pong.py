2aefc52d7b180b8a5feee894286df579e02ab986
Max Lapan <max.lapan@gmail.com>
2017-11-06 09:48:40 +0100

    Revert to the original version

--- ch06/02_dqn_pong.py
+++ ch06/02_dqn_pong.py
@@ -44,7 +44,7 @@ class ExperienceBuffer:
         self.buffer.append(experience)
 
     def sample(self, batch_size):
-        indices = np.random.choice(len(self.buffer), batch_size, replace=True)
+        indices = np.random.choice(len(self.buffer), batch_size, replace=False)
         states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
         return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
                np.array(dones, dtype=np.uint8), np.array(next_states)
