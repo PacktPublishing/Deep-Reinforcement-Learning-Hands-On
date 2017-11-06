0f69a7718d336e60184940a4d1a351cf330984f2
Max Lapan <max.lapan@gmail.com>
2017-11-06 13:28:53 +0100

    Calculate values on held-out set

--- ch07/03_dqn_double.py
+++ ch07/03_dqn_double.py
@@ -2,6 +2,7 @@
 import gym
 import ptan
 import argparse
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -12,8 +13,11 @@ from tensorboardX import SummaryWriter
 
 from lib import dqn_model, common
 
+STATES_TO_EVALUATE = 1000
+EVAL_EVERY_FRAME = 100
 
-def calc_loss(batch, net, tgt_net, gamma, cuda=False):
+
+def calc_loss(batch, net, tgt_net, gamma, cuda=False, double=True):
     states, actions, rewards, dones, next_states = common.unpack_batch(batch)
 
     states_v = Variable(torch.from_numpy(states))
@@ -29,8 +33,11 @@ def calc_loss(batch, net, tgt_net, gamma, cuda=False):
         done_mask = done_mask.cuda()
 
     state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
-    next_state_actions = net(next_states_v).max(1)[1]
-    next_state_values = tgt_net(next_states_v).gather(1, next_state_actions.unsqueeze(-1)).squeeze(-1)
+    if double:
+        next_state_actions = net(next_states_v).max(1)[1]
+        next_state_values = tgt_net(next_states_v).gather(1, next_state_actions.unsqueeze(-1)).squeeze(-1)
+    else:
+        next_state_values = tgt_net(next_states_v).max(1)[0]
     next_state_values[done_mask] = 0.0
     next_state_values.volatile = False
 
@@ -38,16 +45,30 @@ def calc_loss(batch, net, tgt_net, gamma, cuda=False):
     return nn.MSELoss()(state_action_values, expected_state_action_values)
 
 
+def calc_values_of_states(states, net, cuda=False):
+    mean_vals = []
+    for batch in np.array_split(states, 64):
+        states_v = Variable(torch.from_numpy(batch), volatile=True)
+        if cuda:
+            states_v = states_v.cuda()
+        action_values_v = net(states_v)
+        best_action_values_v = action_values_v.max(1)[0]
+        mean_val = best_action_values_v.mean().data.cpu().numpy()[0]
+        mean_vals.append(mean_val)
+    return np.mean(mean_vals)
+
+
 if __name__ == "__main__":
     params = common.HYPERPARAMS['pong']
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("--no-double", default=False, action="store_true", help="Disable double DQN")
     args = parser.parse_args()
 
     env = gym.make(params['env_name'])
     env = ptan.common.wrappers.wrap_dqn(env)
 
-    writer = SummaryWriter(comment="-" + params['run_name'] + "-double")
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-double=" + str(not args.no_double))
     net = dqn_model.DQN(env.observation_space.shape, env.action_space.n)
     if args.cuda:
         net.cuda()
@@ -62,6 +83,7 @@ if __name__ == "__main__":
     optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
 
     frame_idx = 0
+    eval_states = None
 
     with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
         while True:
@@ -76,12 +98,21 @@ if __name__ == "__main__":
 
             if len(buffer) < params['replay_initial']:
                 continue
+            if eval_states is None:
+                eval_states = buffer.sample(STATES_TO_EVALUATE)
+                eval_states = [np.array(transition.state, copy=False) for transition in eval_states]
+                eval_states = np.array(eval_states, copy=False)
 
             optimizer.zero_grad()
             batch = buffer.sample(params['batch_size'])
-            loss_v = calc_loss(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
+            loss_v = calc_loss(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda,
+                               double=not args.no_double)
             loss_v.backward()
             optimizer.step()
 
             if frame_idx % params['target_net_sync'] == 0:
                 tgt_net.sync()
+            if frame_idx % EVAL_EVERY_FRAME == 0:
+                mean_val = calc_values_of_states(eval_states, net, cuda=args.cuda)
+                writer.add_scalar("values_mean", mean_val, frame_idx)
+
