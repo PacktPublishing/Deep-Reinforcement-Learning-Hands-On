a6f56790d3845e240249ad8209579eb4ac267479
Max Lapan <max.lapan@gmail.com>
2017-11-07 14:57:52 +0100

    Longer epsilon decay

--- ch07/07_dqn_distrib.py
+++ ch07/07_dqn_distrib.py
@@ -120,7 +120,7 @@ def calc_loss(batch, net, tgt_net, gamma, cuda=False):
 
 if __name__ == "__main__":
     params = common.HYPERPARAMS['pong']
-#    params['epsilon_frames'] = 200000
+    params['epsilon_frames'] = 200000
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
     args = parser.parse_args()
