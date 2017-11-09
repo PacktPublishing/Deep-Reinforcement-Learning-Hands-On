528acbad0beafd70cc69e2602ce8aef3c90ba226
Max Lapan <max.lapan@gmail.com>
2017-11-09 13:41:46 +0100

    Show both src and projected distribution

--- ch07/adhoc/distr_test.py
+++ ch07/adhoc/distr_test.py
@@ -1,4 +1,6 @@
+import sys
 import numpy as np
+sys.path.append("./")
 
 from lib import common
 
@@ -13,10 +15,15 @@ N_ATOMS = 51
 DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
 
 
-def save_distr(vec, name):
-    plt.cla()
+def save_distr(src, proj, name):
+    plt.clf()
     p = np.arange(Vmin, Vmax+DELTA_Z, DELTA_Z)
-    plt.bar(p, vec, width=0.5)
+    plt.subplot(2, 1, 1)
+    plt.bar(p, src, width=0.5)
+    plt.title("Source")
+    plt.subplot(2, 1, 2)
+    plt.bar(p, proj, width=0.5)
+    plt.title("Projected")
     plt.savefig(name + ".png")
 
 
@@ -27,51 +34,45 @@ if __name__ == "__main__":
     # single peak distribution
     src_hist = np.zeros(shape=(1, N_ATOMS), dtype=np.float32)
     src_hist[0, N_ATOMS//2+1] = 1.0
-    save_distr(src_hist[0], "peak-01")
     proj_hist = common.distr_projection(src_hist, np.array([2], dtype=np.float32), np.array([False]),
                                         Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "peak-02")
+    save_distr(src_hist[0], proj_hist[0], "peak-r=2")
 
     # normal distribution
     data = np.random.normal(size=1000, scale=3)
-    hist = np.histogram(data, bins=np.arange(Vmin - DELTA_Z/2, Vmax + DELTA_Z*3/2, DELTA_Z))
-    save_distr(hist[0], "normal-01")
+    hist = np.histogram(data, normed=True, bins=np.arange(Vmin - DELTA_Z/2, Vmax + DELTA_Z*3/2, DELTA_Z))
 
     src_hist = hist[0]
     proj_hist = common.distr_projection(np.array([src_hist]), np.array([2], dtype=np.float32), np.array([False]),
                                         Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "normal-02")
+    save_distr(hist[0], proj_hist[0], "normal-r=2")
 
     # normal distribution, but done episode
     proj_hist = common.distr_projection(np.array([src_hist]), np.array([2], dtype=np.float32), np.array([True]),
                                         Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "normal-03")
+    save_distr(hist[0], proj_hist[0], "normal-done-r=2")
 
     # clipping for out-of-range distribution
     proj_dist = common.distr_projection(np.array([src_hist]), np.array([10], dtype=np.float32), np.array([False]),
                                         Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_dist[0], "normal-04")
-
-    proj_dist = common.distr_projection(np.array([src_hist]), np.array([10], dtype=np.float32), np.array([False]),
-                                        Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_dist[0], "normal-05")
+    save_distr(hist[0], proj_dist[0], "normal-r=10")
 
     # test both done and not done, unclipped
     proj_hist = common.distr_projection(np.array([src_hist, src_hist]), np.array([2, 2], dtype=np.float32),
                                         np.array([False, True]), Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "both_not_clip-01-incomplete")
-    save_distr(proj_hist[1], "both_not_clip-02-complete")
+    save_distr(src_hist, proj_hist[0], "both_not_clip-01-incomplete")
+    save_distr(src_hist, proj_hist[1], "both_not_clip-02-complete")
 
     # test both done and not done, clipped right
     proj_hist = common.distr_projection(np.array([src_hist, src_hist]), np.array([10, 10], dtype=np.float32),
                                         np.array([False, True]), Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "both_clip-right-01-incomplete")
-    save_distr(proj_hist[1], "both_clip-right-02-complete")
+    save_distr(src_hist, proj_hist[0], "both_clip-right-01-incomplete")
+    save_distr(src_hist, proj_hist[1], "both_clip-right-02-complete")
 
     # test both done and not done, clipped left
     proj_hist = common.distr_projection(np.array([src_hist, src_hist]), np.array([-10, -10], dtype=np.float32),
                                         np.array([False, True]), Vmin, Vmax, N_ATOMS, gamma=0.9)
-    save_distr(proj_hist[0], "both_clip-left-01-incomplete")
-    save_distr(proj_hist[1], "both_clip-left-02-complete")
+    save_distr(src_hist, proj_hist[0], "both_clip-left-01-incomplete")
+    save_distr(src_hist, proj_hist[1], "both_clip-left-02-complete")
 
     pass
