9e01d5e3e101c92de5172699871f521d1878b0c6
Max Lapan <max.lapan@gmail.com>
2017-11-09 19:15:46 +0100

    Rainbow version

---
+++ ch07/08_dqn_rainbow.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+import gym
+import ptan
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torch.optim as optim
+
+from tensorboardX import SummaryWriter
+
+from lib import dqn_model, common
+
+# n-step
+REWARD_STEPS = 2
+
+# priority replay
+PRIO_REPLAY_ALPHA = 0.6
+BETA_START = 0.4
+BETA_FRAMES = 100000
+
+# C51
+Vmax = 10
+Vmin = -10
+N_ATOMS = 51
+DELTA_Z = (Vmax - Vmin) / (N_ATOMS - 1)
+
+
+class RainbowDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(RainbowDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc_val = nn.Sequential(
+            dqn_model.NoisyLinear(conv_out_size, 512),
+            nn.ReLU(),
+            dqn_model.NoisyLinear(512, N_ATOMS)
+        )
+
+        self.fc_adv = nn.Sequential(
+            dqn_model.NoisyLinear(conv_out_size, 512),
+            nn.ReLU(),
+            dqn_model.NoisyLinear(512, n_actions * N_ATOMS)
+        )
+
+        self.register_buffer("supports", torch.arange(Vmin, Vmax, DELTA_Z))
+        self.softmax = nn.Softmax()
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(batch_size, -1)
+        val_out = self.fc_val(conv_out).view(batch_size, 1, N_ATOMS)
+        adv_out = self.fc_adv(conv_out).view(batch_size, -1, N_ATOMS)
+        adv_mean = adv_out.mean(dim=1, keepdim=True)
+        return val_out + adv_out - adv_mean
+
+    def both(self, x):
+        cat_out = self(x)
+        probs = self.apply_softmax(cat_out)
+        weights = probs * Variable(self.supports, volatile=True)
+        res = weights.sum(dim=2)
+        return cat_out, res
+
+    def qvals(self, x):
+        return self.both(x)[1]
+
+    def apply_softmax(self, t):
+        return self.softmax(t.view(-1, N_ATOMS)).view(t.size())
+
+
+def calc_loss(batch, batch_weights, net, tgt_net, gamma, cuda=False):
+    states, actions, rewards, dones, next_states = common.unpack_batch(batch)
+    batch_size = len(batch)
+
+    states_v = Variable(torch.from_numpy(states))
+    actions_v = Variable(torch.from_numpy(actions))
+    next_states_v = Variable(torch.from_numpy(next_states), volatile=True)
+    batch_weights_v = Variable(torch.from_numpy(batch_weights))
+    if cuda:
+        states_v = states_v.cuda()
+        actions_v = actions_v.cuda()
+        next_states_v = next_states_v.cuda()
+        batch_weights_v = batch_weights_v.cuda()
+
+    # next state distribution
+    # dueling arch -- actions from main net, distr from tgt_net
+    next_qvals_v = net.qvals(next_states_v)
+    next_actions = next_qvals_v.max(1)[1].data.cpu().numpy()
+    next_distr_v = tgt_net(next_states_v)
+    next_distr = tgt_net.apply_softmax(next_distr_v).data.cpu().numpy()
+
+    next_best_distr = next_distr[range(batch_size), next_actions]
+    dones = dones.astype(np.bool)
+
+    # project our distribution using Bellman update
+    proj_distr = common.distr_projection(next_best_distr, rewards, dones, Vmin, Vmax, N_ATOMS, gamma)
+
+    # calculate net output
+    distr_v = net(states_v)
+    state_action_values = distr_v[range(batch_size), actions_v.data]
+    state_log_sm_v = F.log_softmax(state_action_values)
+    proj_distr_v = Variable(torch.from_numpy(proj_distr))
+    if cuda:
+        proj_distr_v = proj_distr_v.cuda()
+
+    loss_v = -state_log_sm_v * proj_distr_v
+    loss_v = batch_weights_v * loss_v.sum(dim=1)
+    return loss_v.mean(), loss_v
+
+
+
+if __name__ == "__main__":
+    params = common.HYPERPARAMS['pong']
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    args = parser.parse_args()
+
+    env = gym.make(params['env_name'])
+    env = ptan.common.wrappers.wrap_dqn(env)
+
+    writer = SummaryWriter(comment="-" + params['run_name'] + "-rainbow")
+    net = RainbowDQN(env.observation_space.shape, env.action_space.n)
+    if args.cuda:
+        net.cuda()
+
+    tgt_net = ptan.agent.TargetNet(net)
+    agent = ptan.agent.DQNAgent(lambda x: net.qvals(x), ptan.actions.ArgmaxActionSelector(), cuda=args.cuda)
+
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=params['gamma'], steps_count=REWARD_STEPS)
+    buffer = ptan.experience.PrioReplayBuffer(exp_source, params['replay_size'], PRIO_REPLAY_ALPHA)
+    optimizer = optim.Adam(net.parameters(), lr=params['learning_rate'])
+
+    frame_idx = 0
+    beta = BETA_START
+
+    with common.RewardTracker(writer, params['stop_reward']) as reward_tracker:
+        while True:
+            frame_idx += 1
+            buffer.populate(1)
+            beta = min(1.0, BETA_START + frame_idx * (1.0 - BETA_START) / BETA_FRAMES)
+
+            new_rewards = exp_source.pop_total_rewards()
+            if new_rewards:
+                if reward_tracker.reward(new_rewards[0], frame_idx):
+                    break
+
+            if len(buffer) < params['replay_initial']:
+                continue
+
+            optimizer.zero_grad()
+            batch, batch_indices, batch_weights = buffer.sample(params['batch_size'], beta)
+            loss_v, sample_prios_v = calc_loss(batch, batch_weights, net, tgt_net.target_model,
+                                               params['gamma'] ** REWARD_STEPS, cuda=args.cuda)
+            loss_v.backward()
+            optimizer.step()
+            buffer.update_priorities(batch_indices, sample_prios_v.data.cpu().numpy())
+
+            if frame_idx % params['target_net_sync'] == 0:
+                tgt_net.sync()
