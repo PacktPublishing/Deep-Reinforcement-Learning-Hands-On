8a5ec164bc9d9023a624478c71993af1cd326309
Max Lapan <max.lapan@gmail.com>
2017-11-12 18:27:47 +0100

    Proper 100 rewards

--- ch08/lib/common.py
+++ ch08/lib/common.py
@@ -33,7 +33,8 @@ class RewardTracker:
         speed = (frame - self.ts_frame) / (time.time() - self.ts)
         self.ts_frame = frame
         self.ts = time.time()
-        mean_reward = np.mean(self.total_rewards[-100:])
+        last_steps = max(1, int(np.ceil(100 / self.group_rewards)))
+        mean_reward = np.mean(self.total_rewards[-last_steps:])
         epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon
         print("%d: done %d games, mean reward %.3f, speed %.2f f/s%s" % (
             frame, len(self.total_rewards)*self.group_rewards, mean_reward, speed, epsilon_str
