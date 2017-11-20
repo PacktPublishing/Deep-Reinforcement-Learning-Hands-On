56fdef00c1d8371ae3e49c5293b934d3a5e2a5d4
Max Lapan <max.lapan@gmail.com>
2017-11-20 21:26:24 +0100

    larger model try

--- ch08/lib/models.py
+++ ch08/lib/models.py
@@ -91,3 +91,51 @@ class DQNConv1D(nn.Module):
         val = self.fc_val(conv_out)
         adv = self.fc_adv(conv_out)
         return val + adv - adv.mean()
+
+
+class DQNConv1DLarge(nn.Module):
+    def __init__(self, shape, actions_n):
+        super(DQNConv1DLarge, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv1d(shape[0], 32, 3),
+            nn.MaxPool1d(3, 2),
+            nn.ReLU(),
+            nn.Conv1d(32, 32, 3),
+            nn.MaxPool1d(3, 2),
+            nn.ReLU(),
+            nn.Conv1d(32, 32, 3),
+            nn.MaxPool1d(3, 2),
+            nn.ReLU(),
+            nn.Conv1d(32, 32, 3),
+            nn.MaxPool1d(3, 2),
+            nn.ReLU(),
+            nn.Conv1d(32, 32, 3),
+            nn.ReLU(),
+            nn.Conv1d(32, 32, 3),
+            nn.ReLU(),
+        )
+
+        out_size = self._get_conv_out(shape)
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+        self.fc_adv = nn.Sequential(
+            nn.Linear(out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, actions_n)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        val = self.fc_val(conv_out)
+        adv = self.fc_adv(conv_out)
+        return val + adv - adv.mean()
