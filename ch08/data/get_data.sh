374c1527507c77b04bcef935a26bc2d3103d5cb4
Max Lapan <max.lapan@gmail.com>
2017-11-24 15:05:09 +0100

    Data location

--- ch08/data/get_data.sh
+++ ch08/data/get_data.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-# TODO: find a place to put dataset, currently at http://ioremap.net/tmp/stocks.tar.gz
+FNAME=ch08-small-quotes.tgz
+rm -f $FNAME
+wget https://www.dropbox.com/s/z2qt7tmylcp18f7/ch08-small-quotes.tgz && tar xvf $FNAME && rm -f $FNAME
