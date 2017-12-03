973cbdfaab3d4d5f79ebe2b489768588b9f642ac
Max Lapan <max.lapan@gmail.com>
2017-12-03 12:24:13 +0100

    Baseline version of REINFORCE

---
+++ ch09/03_cartpole_reinforce_baseline.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+import gym
+import ptan
+import numpy as np
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+GAMMA = 0.99
+LEARNING_RATE = 0.01
+EPISODES_TO_TRAIN = 4
+
+
+class PGN(nn.Module):
+    def __init__(self, input_size, n_actions):
+        super(PGN, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def calc_qvals(rewards):
+    res = []
+    sum_r = 0.0
+    for r in reversed(rewards):
+        sum_r *= GAMMA
+        sum_r += r
+        res.append(sum_r)
+    res = list(reversed(res))
+    mean_q = np.mean(res)
+    return [q - mean_q for q in res]
+
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    writer = SummaryWriter(comment="-cartpole-reinforce-baseline")
+
+    net = PGN(env.observation_space.shape[0], env.action_space.n)
+    print(net)
+
+    agent = ptan.agent.PolicyAgent(net, preprocessor=ptan.agent.float32_preprocessor,
+                                   apply_softmax=True)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(env, agent, gamma=GAMMA)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
+    total_rewards = []
+    step_idx = 0
+    done_episodes = 0
+
+    batch_episodes = 0
+    batch_states, batch_actions, batch_qvals = [], [], []
+    cur_states, cur_actions, cur_rewards = [], [], []
+
+    for step_idx, exp in enumerate(exp_source):
+        cur_states.append(exp.state)
+        cur_actions.append(int(exp.action))
+        cur_rewards.append(exp.reward)
+
+        if exp.last_state is None:
+            batch_states.extend(cur_states)
+            batch_actions.extend(cur_actions)
+            batch_qvals.extend(calc_qvals(cur_rewards))
+            cur_states.clear()
+            cur_actions.clear()
+            cur_rewards.clear()
+            batch_episodes += 1
+
+        # handle new rewards
+        new_rewards = exp_source.pop_total_rewards()
+        if new_rewards:
+            done_episodes += 1
+            reward = new_rewards[0]
+            total_rewards.append(reward)
+            mean_rewards = float(np.mean(total_rewards[-100:]))
+            print("%d: reward: %6.2f, mean_100: %6.2f, episodes: %d" % (
+                step_idx, reward, mean_rewards, done_episodes))
+            writer.add_scalar("reward", reward, step_idx)
+            writer.add_scalar("reward_100", mean_rewards, step_idx)
+            writer.add_scalar("episodes", done_episodes, step_idx)
+            if mean_rewards > 195:
+                print("Solved in %d steps and %d episodes!" % (step_idx, done_episodes))
+                break
+
+        if batch_episodes < EPISODES_TO_TRAIN:
+            continue
+
+        states_v = Variable(torch.from_numpy(np.array(batch_states, dtype=np.float32)))
+        batch_actions_t = torch.LongTensor(batch_actions)
+        batch_qvals_v = Variable(torch.FloatTensor(batch_qvals))
+
+        optimizer.zero_grad()
+        logits_v = net(states_v)
+        log_prob_v = F.log_softmax(logits_v)
+        log_prob_actions_v = batch_qvals_v * log_prob_v[range(len(batch_states)), batch_actions_t]
+        loss_v = -log_prob_actions_v.mean()
+
+        loss_v.backward()
+        optimizer.step()
+
+        batch_episodes = 0
+        batch_states.clear()
+        batch_actions.clear()
+        batch_qvals.clear()
+
+    writer.close()
