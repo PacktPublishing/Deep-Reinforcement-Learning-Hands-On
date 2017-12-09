d57851b9772097909ffc6e8b1d2bf47d5a1d1d86
Max Lapan <max.lapan@gmail.com>
2017-12-09 23:23:04 +0100

    Batch unpacking

--- ch10/01_pong_a2c.py
+++ ch10/01_pong_a2c.py
@@ -62,6 +62,48 @@ class AtariA2C(nn.Module):
         return self.policy(conv_out), self.value(conv_out)
 
 
+def unpack_batch(batch, net, cuda=False):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = Variable(torch.from_numpy(np.array(states, copy=False)))
+    actions_t = torch.LongTensor(actions)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_t = actions_t.cuda()
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = Variable(torch.from_numpy(np.array(last_states, copy=False)), volatile=True)
+        if cuda:
+            last_states_v = last_states_v.cuda()
+        last_vals_v = net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
+
+    ref_vals_v = Variable(torch.from_numpy(rewards_np))
+    if cuda:
+        ref_vals_v = ref_vals_v.cuda()
+
+    return states_v, actions_t, ref_vals_v
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
@@ -85,61 +127,36 @@ if __name__ == "__main__":
     done_episodes = 0
     train_step_idx = 0
 
-    batch_states, batch_actions, batch_rewards, batch_last_states, batch_dones = [], [], [], [], []
+    batch = []
     m_values, m_batch_rewards, m_loss_entropy, m_loss_policy, m_loss_total, m_loss_value = [], [], [], [], [], []
     m_adv = []
     m_grad_max, m_grad_mean = [], []
 
     with common.RewardTracker(writer, stop_reward=18) as tracker:
         for step_idx, exp in enumerate(exp_source):
-            batch_states.append(np.array(exp.state, copy=False))
-            batch_actions.append(int(exp.action))
-            batch_rewards.append(exp.reward)
-            batch_dones.append(exp.last_state is None)
-            if exp.last_state is not None:
-                batch_last_states.append(np.array(exp.last_state, copy=False))
+            batch.append(exp)
+
             # handle new rewards
             new_rewards = exp_source.pop_total_rewards()
             if new_rewards:
                 if tracker.reward(new_rewards[0], step_idx):
                     break
 
-            if len(batch_states) < BATCH_SIZE:
+            if len(batch) < BATCH_SIZE:
                 continue
 
             train_step_idx += 1
-            states_v = Variable(torch.from_numpy(np.array(batch_states, copy=False)))
-            if not all(batch_dones):
-                last_states_v = Variable(torch.from_numpy(np.array(batch_last_states, copy=False)))
-            else:
-                last_states_v = None
-            batch_actions_t = torch.LongTensor(batch_actions)
-            if args.cuda:
-                states_v = states_v.cuda()
-                if last_states_v is not None:
-                    last_states_v = last_states_v.cuda()
-                batch_actions_t = batch_actions_t.cuda()
-
-            batch_rewards_np = np.array(batch_rewards, dtype=np.float32)
-            if last_states_v is not None:
-                last_values = net(last_states_v)[1].data.cpu().numpy()
-                batch_dones_np = np.array(batch_dones)
-                batch_rewards_np[~batch_dones_np] += (GAMMA ** REWARD_STEPS) * last_values[:, 0]
-
-            batch_rewards_v = Variable(torch.from_numpy(batch_rewards_np))
-            if args.cuda:
-                batch_rewards_v = batch_rewards_v.cuda()
+            states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=args.cuda)
 
             optimizer.zero_grad()
             logits_v, value_v = net(states_v)
 
-            loss_value_v = F.mse_loss(value_v, batch_rewards_v)
+            loss_value_v = F.mse_loss(value_v, vals_ref_v)
 
             log_prob_v = F.log_softmax(logits_v)
-            adv_v = batch_rewards_v - value_v.detach()
-            log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), batch_actions_t]
+            adv_v = vals_ref_v - value_v.detach()
+            log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
             loss_policy_v = -log_prob_actions_v.mean()
-
 
             prob_v = F.softmax(logits_v)
             entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
@@ -149,7 +166,7 @@ if __name__ == "__main__":
 
             m_adv.append(adv_v.mean().data.cpu().numpy()[0])
             m_values.append(value_v.mean().data.cpu().numpy()[0])
-            m_batch_rewards.append(np.mean(batch_rewards))
+            m_batch_rewards.append(vals_ref_v.mean().data.cpu().numpy()[0])
             m_loss_entropy.append(entropy_loss_v.data.cpu().numpy()[0])
             m_loss_policy.append(loss_policy_v.data.cpu().numpy()[0])
             m_loss_total.append(loss_v.data.cpu().numpy()[0])
@@ -179,10 +196,6 @@ if __name__ == "__main__":
                 m_adv = []
                 m_grad_max, m_grad_mean = [], []
 
-            batch_states.clear()
-            batch_actions.clear()
-            batch_rewards.clear()
-            batch_last_states.clear()
-            batch_dones.clear()
+            batch.clear()
 
     writer.close()
