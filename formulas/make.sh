214528bbb4d17a8850787a5f3897ff6be49c9127
Max Lapan <max.lapan@gmail.com>
2017-12-16 17:17:57 +0100

    Generate of full doc

--- formulas/make.sh
+++ formulas/make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 [ -d ch01 ] || mkdir ch01
-pdflatex ch01.tex
+# create full doc
+pdflatex -jobname=ch01/ch01 ch01.tex
+# create images
+pdflatex "\def\ispreview{1} \input{ch01.tex}"
 convert -density 300 ch01.pdf -quality 90 ch01/ch01.png
