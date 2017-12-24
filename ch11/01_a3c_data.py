b3453c6bcd505d208e913175d4ffef1ae0849b93
Max Lapan <max.lapan@gmail.com>
2017-12-24 11:29:48 +0100

    Back to simplicity

--- ch11/01_a3c_data.py
+++ ch11/01_a3c_data.py
@@ -3,7 +3,6 @@ import gym
 import ptan
 import numpy as np
 import argparse
-import collections
 from tensorboardX import SummaryWriter
 
 import torch
@@ -23,63 +22,65 @@ BATCH_SIZE = 128
 REWARD_STEPS = 4
 CLIP_GRAD = 0.1
 
-PROCESSES_COUNT = 5
-NUM_ENVS = 10
+PROCESSES_COUNT = 4
+NUM_ENVS = 12
+
 
 def make_env():
     return ptan.common.wrappers.wrap_dqn(gym.make("PongNoFrameskip-v4"))
 
 
-TrainEntry = collections.namedtuple('TrainEntry', field_names=['state', 'q', 'action'])
-
-
-class CachingA2CAgent(ptan.agent.BaseAgent):
-    def __init__(self, model, cuda=False, preprocessor=ptan.agent.default_states_preprocessor):
-        self.model = model
-        self.cuda = cuda
-        self.values_cache = {}
-        self.action_selector = ptan.actions.ProbabilityActionSelector()
-        self.preprocessor = preprocessor
-
-    def __call__(self, states, agent_states=None):
-        if agent_states is None:
-            agent_states = [None] * states.shape[0]
-        if self.preprocessor is not None:
-            prep_states = self.preprocessor(states)
-        v = Variable(torch.from_numpy(prep_states))
-        if self.cuda:
-            v = v.cuda()
-        probs_v, values_v = self.model(v)
-        probs_v = F.softmax(probs_v)
-        probs = probs_v.data.cpu().numpy()
-        actions = self.action_selector(probs)
-
-        values = values_v.data.cpu().numpy().squeeze()
-        for state, value in zip(states, values):
-            self.values_cache[id(state)] = value
-
-        return np.array(actions), agent_states
-
-
 def data_func(net, cuda, train_queue):
     envs = [make_env() for _ in range(NUM_ENVS)]
-    agent = CachingA2CAgent(net, cuda=cuda)
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], cuda=cuda, apply_softmax=True)
     exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
 
     for exp in exp_source:
-        if exp.last_state is None:
-            print("Done!")
-            entry = TrainEntry(state=exp.state, q=exp.reward, action=exp.action)
-            train_queue.put(entry)
-            continue
-
-        Q = exp.reward
-        state = exp.state
-        Q += GAMMA ** REWARD_STEPS * agent.values_cache[id(state)]
-        entry = TrainEntry(state=exp.state, q=Q, action=exp.action)
-        train_queue.put(entry)
+        train_queue.put(exp)
 
     train_queue.put(None)
+
+
+def unpack_batch(batch, net, cuda=False):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = Variable(torch.from_numpy(np.array(states, copy=False)))
+    actions_t = torch.LongTensor(actions)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_t = actions_t.cuda()
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = Variable(torch.from_numpy(np.array(last_states, copy=False)), volatile=True)
+        if cuda:
+            last_states_v = last_states_v.cuda()
+        last_vals_v = net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
+
+    ref_vals_v = Variable(torch.from_numpy(rewards_np))
+    if cuda:
+        ref_vals_v = ref_vals_v.cuda()
+
+    return states_v, actions_t, ref_vals_v
 
 
 if __name__ == "__main__":
@@ -105,9 +106,7 @@ if __name__ == "__main__":
         data_proc.start()
         data_proc_list.append(data_proc)
 
-    batch_states = []
-    batch_qs = []
-    batch_actions = []
+    batch = []
     batch_idx = 0
 
     with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
@@ -115,29 +114,21 @@ if __name__ == "__main__":
             train_entry = train_queue.get()
             if train_entry is None:
                 break
-            batch_states.append(np.array(train_entry.state, copy=False))
-            batch_qs.append(float(train_entry.q))
-            batch_actions.append(int(train_entry.action))
-
-            if len(batch_states) < BATCH_SIZE:
+            batch.append(train_entry)
+            if len(batch) < BATCH_SIZE:
                 continue
 
             batch_idx += 1
-            states_v = Variable(torch.from_numpy(np.array(batch_states, copy=False)))
-            qs_v = Variable(torch.FloatTensor(batch_qs))
-            actions_t = torch.LongTensor(batch_actions)
-            if args.cuda:
-                states_v = states_v.cuda()
-                qs_v = qs_v.cuda()
-                actions_t = actions_t.cuda()
+            states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=args.cuda)
+            batch.clear()
 
             optimizer.zero_grad()
             logits_v, value_v = net(states_v)
 
-            loss_value_v = F.mse_loss(value_v, qs_v)
+            loss_value_v = F.mse_loss(value_v, vals_ref_v)
 
             log_prob_v = F.log_softmax(logits_v)
-            adv_v = qs_v - value_v.detach()
+            adv_v = vals_ref_v - value_v.detach()
             log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
             loss_policy_v = -log_prob_actions_v.mean()
 
@@ -160,7 +151,7 @@ if __name__ == "__main__":
 
             tb_tracker.track("advantage", adv_v, batch_idx)
             tb_tracker.track("values", value_v, batch_idx)
-            tb_tracker.track("batch_rewards", qs_v, batch_idx)
+            tb_tracker.track("batch_rewards", vals_ref_v, batch_idx)
             tb_tracker.track("loss_entropy", entropy_loss_v, batch_idx)
             tb_tracker.track("loss_policy", loss_policy_v, batch_idx)
             tb_tracker.track("loss_value", loss_value_v, batch_idx)
@@ -169,9 +160,5 @@ if __name__ == "__main__":
             tb_tracker.track("grad_l2", np.sqrt(np.mean(np.square(grads))), batch_idx)
             tb_tracker.track("grad_max", np.max(np.abs(grads)), batch_idx)
             tb_tracker.track("grad_var", np.var(grads), batch_idx)
-
-            batch_states.clear()
-            batch_qs.clear()
-            batch_actions.clear()
 
 pass
