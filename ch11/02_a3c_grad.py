79ecbd07f6f8c966a93a8e406fad31203a441aeb
Max Lapan <max.lapan@gmail.com>
2017-12-27 20:39:52 +0100

    Add capacity to the queues

--- ch11/02_a3c_grad.py
+++ ch11/02_a3c_grad.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
 
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
 
-    train_queue = mp.Queue(maxsize=PROCESSES_COUNT)
+    train_queue = mp.Queue(maxsize=PROCESSES_COUNT*10)
     data_proc_list = []
     tgt_nets = []
     for proc_idx in range(PROCESSES_COUNT):
