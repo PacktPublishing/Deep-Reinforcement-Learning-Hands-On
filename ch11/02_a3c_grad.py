8dbca8389604c478e669de906d33d871ef6b7a67
Max Lapan <max.lapan@gmail.com>
2017-12-27 20:38:44 +0100

    Simplification

--- ch11/02_a3c_grad.py
+++ ch11/02_a3c_grad.py
@@ -3,7 +3,6 @@ import gym
 import ptan
 import numpy as np
 import argparse
-import collections
 from tensorboardX import SummaryWriter
 
 import torch
@@ -28,6 +27,7 @@ NUM_ENVS = 15
 GRAD_BATCH = 4
 TRAIN_BATCH = 16
 
+
 if True:
     ENV_NAME = "PongNoFrameskip-v4"
     NAME = 'pong'
@@ -41,71 +41,61 @@ else:
 def make_env():
     return ptan.common.wrappers.wrap_dqn(gym.make(ENV_NAME))
 
-TotalReward = collections.namedtuple('TotalReward', field_names=['reward'])
-TBValue = collections.namedtuple('TBValue', field_names=['name', 'value'])
 
-
-class TBQueueWriter:
-    def __init__(self, queue):
-        self.queue = queue
-
-    def add_scalar(self, name, value, iter_idx):
-        self.queue.put(TBValue(name=name, value=value))
-
-    def close(self):
-        pass
-
-
-def data_func(net, cuda, train_queue, batch_size=GRAD_BATCH):
+def data_func(proc_name, net, cuda, train_queue, batch_size=GRAD_BATCH):
     envs = [make_env() for _ in range(NUM_ENVS)]
 
     agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], cuda=cuda, apply_softmax=True)
     exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
 
     batch = []
+    frame_idx = 0
+    writer = SummaryWriter(comment=proc_name)
 
-    with ptan.common.utils.TBMeanTracker(TBQueueWriter(train_queue), batch_size=100) as tb_tracker:
-        for exp in exp_source:
-            new_rewards = exp_source.pop_total_rewards()
-            if new_rewards:
-                train_queue.put(TotalReward(reward=np.mean(new_rewards)))
+    with common.RewardTracker(writer, stop_reward=REWARD_BOUND) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=100) as tb_tracker:
+            for exp in exp_source:
+                frame_idx += 1
+                new_rewards = exp_source.pop_total_rewards()
+                if new_rewards and tracker.reward(new_rewards[0], frame_idx):
+                    break
 
-            batch.append(exp)
-            if len(batch) < batch_size:
-                continue
+                batch.append(exp)
+                if len(batch) < batch_size:
+                    continue
 
-            states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=cuda)
-            batch.clear()
+                states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=cuda)
+                batch.clear()
 
-            net.zero_grad()
-            logits_v, value_v = net(states_v)
+                net.zero_grad()
+                logits_v, value_v = net(states_v)
 
-            loss_value_v = F.mse_loss(value_v, vals_ref_v)
+                loss_value_v = F.mse_loss(value_v, vals_ref_v)
 
-            log_prob_v = F.log_softmax(logits_v)
-            adv_v = vals_ref_v - value_v.detach()
-            log_prob_actions_v = adv_v * log_prob_v[range(batch_size), actions_t]
-            loss_policy_v = -log_prob_actions_v.mean()
+                log_prob_v = F.log_softmax(logits_v)
+                adv_v = vals_ref_v - value_v.detach()
+                log_prob_actions_v = adv_v * log_prob_v[range(batch_size), actions_t]
+                loss_policy_v = -log_prob_actions_v.mean()
 
-            prob_v = F.softmax(logits_v)
-            entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+                prob_v = F.softmax(logits_v)
+                entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
 
-            # apply entropy and value gradients
-            loss_v = entropy_loss_v + loss_value_v + loss_policy_v
-            loss_v.backward()
+                # apply entropy and value gradients
+                loss_v = entropy_loss_v + loss_value_v + loss_policy_v
+                loss_v.backward()
 
-            tb_tracker.track("advantage", adv_v, 0)
-            tb_tracker.track("values", value_v, 0)
-            tb_tracker.track("batch_rewards", vals_ref_v, 0)
-            tb_tracker.track("loss_entropy", entropy_loss_v, 0)
-            tb_tracker.track("loss_policy", loss_policy_v, 0)
-            tb_tracker.track("loss_value", loss_value_v, 0)
-            tb_tracker.track("loss_total", loss_v, 0)
+                tb_tracker.track("advantage", adv_v, frame_idx)
+                tb_tracker.track("values", value_v, frame_idx)
+                tb_tracker.track("batch_rewards", vals_ref_v, frame_idx)
+                tb_tracker.track("loss_entropy", entropy_loss_v, frame_idx)
+                tb_tracker.track("loss_policy", loss_policy_v, frame_idx)
+                tb_tracker.track("loss_value", loss_value_v, frame_idx)
+                tb_tracker.track("loss_total", loss_v, frame_idx)
 
-            # gather gradients
-            grads = [param.grad.data.cpu().numpy() if param.grad is not None else None
-                     for param in net.parameters()]
-            train_queue.put(grads)
+                # gather gradients
+                grads = [param.grad.data.cpu().numpy() if param.grad is not None else None
+                         for param in net.parameters()]
+                train_queue.put(grads)
 
     train_queue.put(None)
 
@@ -159,8 +149,6 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--name", required=True, help="Name of the run")
     args = parser.parse_args()
 
-    writer = SummaryWriter(comment="-a3c-grad_" + NAME + "_" + args.name)
-
     env = make_env()
     net = common.AtariA2C(env.observation_space.shape, env.action_space.n)
     if args.cuda:
@@ -171,10 +159,11 @@ if __name__ == "__main__":
     train_queue = mp.Queue(maxsize=PROCESSES_COUNT)
     data_proc_list = []
     tgt_nets = []
-    for _ in range(PROCESSES_COUNT):
+    for proc_idx in range(PROCESSES_COUNT):
         tgt_net = ptan.agent.TargetNet(net)
         tgt_nets.append(tgt_net)
-        data_proc = mp.Process(target=data_func, args=(tgt_net.target_model, args.cuda, train_queue))
+        proc_name = "-a3c-grad_" + NAME + "_" + args.name + "#%d" % proc_idx
+        data_proc = mp.Process(target=data_func, args=(proc_name, tgt_net.target_model, args.cuda, train_queue))
         data_proc.start()
         data_proc_list.append(data_proc)
 
@@ -182,37 +171,28 @@ if __name__ == "__main__":
     step_idx = 0
     grad_buffer = None
 
-    with common.RewardTracker(writer, stop_reward=REWARD_BOUND) as tracker:
-        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
-            while True:
-                train_entry = train_queue.get()
-                if train_entry is None:
-                    break
-                if isinstance(train_entry, TotalReward):
-                    if tracker.reward(train_entry.reward, step_idx):
-                        break
-                    continue
-                elif isinstance(train_entry, TBValue):
-                    writer.add_scalar(train_entry.name, train_entry.value, step_idx * GRAD_BATCH)
-                    continue
+    while True:
+        train_entry = train_queue.get()
+        if train_entry is None:
+            break
 
-                step_idx += 1
+        step_idx += 1
 
-                if grad_buffer is None:
-                    grad_buffer = train_entry
-                else:
-                    for tgt_grad, grad in zip(grad_buffer, train_entry):
-                        tgt_grad += grad
+        if grad_buffer is None:
+            grad_buffer = train_entry
+        else:
+            for tgt_grad, grad in zip(grad_buffer, train_entry):
+                tgt_grad += grad
 
-                if step_idx % TRAIN_BATCH == 0:
-                    for param, grad in zip(net.parameters(), grad_buffer):
-                        grad_v = Variable(torch.from_numpy(grad))
-                        if args.cuda:
-                            grad_v = grad_v.cuda()
-                        param.grad = grad_v
+        if step_idx % TRAIN_BATCH == 0:
+            for param, grad in zip(net.parameters(), grad_buffer):
+                grad_v = Variable(torch.from_numpy(grad))
+                if args.cuda:
+                    grad_v = grad_v.cuda()
+                param.grad = grad_v
 
-                    nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
-                    optimizer.step()
-                    for tgt_net in tgt_nets:
-                        tgt_net.sync()
-                    grad_buffer = None
+            nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+            optimizer.step()
+            for tgt_net in tgt_nets:
+                tgt_net.sync()
+            grad_buffer = None
