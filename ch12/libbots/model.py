c73e9997bbc0fe8523a7716f8408bbf53f68d364
Max Lapan <max.lapan@gmail.com>
2018-01-03 13:48:51 +0100

    Encoding done

--- ch12/libbots/model.py
+++ ch12/libbots/model.py
@@ -1,5 +1,9 @@
+import numpy as np
+
 import torch
 import torch.nn as nn
+import torch.nn.utils.rnn as rnn_utils
+from torch.autograd import Variable
 
 
 class PhraseModel(nn.Module):
@@ -15,3 +19,23 @@ class PhraseModel(nn.Module):
     def forward(self, x):
         _, hid = self.encoder(x)
         return hid
+
+
+def pack_batch(batch, embeddings, cuda=False):
+    assert isinstance(batch, list)
+    # Sort descending (CuDNN requirements)
+    batch.sort(key=lambda s: len(s[0]), reverse=True)
+    input_idx, output_idx = zip(*batch)
+    # create padded matrix of inputs
+    lens = list(map(len, input_idx))
+    input_mat = np.zeros((len(batch), lens[0]), dtype=np.int64)
+    for idx, x in enumerate(input_idx):
+        input_mat[idx, :len(x)] = x
+    input_v = Variable(torch.from_numpy(input_mat))
+    if cuda:
+        input_v = input_v.cuda()
+    input_seq = rnn_utils.pack_padded_sequence(input_v, lens, batch_first=True)
+    # lookup embeddings
+    r = embeddings(input_seq.data)
+    emb_input_seq = rnn_utils.PackedSequence(data=r, batch_sizes=lens)
+    return emb_input_seq, output_idx
--- ch12/train_model.py
+++ ch12/train_model.py
@@ -4,8 +4,12 @@ import logging
 
 from libbots import subtitles, data, model
 
+import torch
+import torch.nn as nn
+
 DATA_FILE = "data/OpenSubtitles/en/Action/2005/365_100029_136606_sin_city.xml.gz"
 HIDDEN_STATE_SIZE = 32
+BATCH_SIZE = 16
 
 log = logging.getLogger("train")
 
@@ -23,8 +27,19 @@ if __name__ == "__main__":
     train_data = data.dialogues_to_train(dialogues, emb_dict)
     log.info("Training data converted, got %d samples", len(train_data))
 
+    # initialize embedding lookup table
+    embeddings = nn.Embedding(num_embeddings=emb.shape[0], embedding_dim=emb.shape[1])
+    embeddings.weight.data.copy_(torch.from_numpy(emb))
+    embeddings.weight.requires_grad = False
+    if args.cuda:
+        embeddings.cuda()
+
     net = model.PhraseModel(emb_size=emb.shape[1], dict_size=emb.shape[0], hid_size=HIDDEN_STATE_SIZE)
     if args.cuda:
         net.cuda()
     log.info("Model: %s", net)
+
+    batch = train_data[:BATCH_SIZE]
+    input_seq, out_idx = model.pack_batch(batch, embeddings, cuda=args.cuda)
+    r = net(input_seq)
     pass
