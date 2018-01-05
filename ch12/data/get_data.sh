19455712be0c64c038a4ff1ac18e22dd5f417f2d
Max Lapan <max.lapan@gmail.com>
2018-01-05 09:55:32 +0100

    Cornell movie dialogs

--- ch12/data/get_data.sh
+++ ch12/data/get_data.sh
@@ -10,3 +10,6 @@ fi
 [ ! -f glove.6B.zip ] && wget http://nlp.stanford.edu/data/glove.6B.zip
 [ ! -f glove.6B.100d.txt ] && unzip glove.6B.zip glove.6B.100d.txt
 
+[ ! -f cornell_movie_dialogs_corpus.zip ] && wget http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip
+[ ! -d "cornell movie-dialogs corpus" ] && unzip -x cornell_movie_dialogs_corpus.zip && rm -rf __MACOSX
+[ ! -d cornell ] && mv "cornell movie-dialogs corpus" cornell
