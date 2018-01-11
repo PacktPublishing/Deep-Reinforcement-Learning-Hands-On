985317ec522078c7db57754ddd19744ba383cdc8
Max Lapan <max.lapan@gmail.com>
2018-01-11 16:23:07 +0100

    reorg

--- ch12/tools/cor_reader.py
+++ ch12/tools/cor_reader.py
@@ -10,7 +10,8 @@ from libbots import cornell, data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--genre", help="Genre to show dialogs from")
+    parser.add_argument("-g", "--genre", default='', help="Genre to show dialogs from")
+    parser.add_argument("--show-dials", action='store_true', default=False, help="Display dialogs")
     args = parser.parse_args()
 
     genre_counts = collections.Counter()
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     for g, count in genre_counts.most_common():
         print("%s: %d" % (g, count))
 
-    if args.genre is not None:
+    if args.show_dials is not None:
         dials = cornell.load_dialogues(genre_filter=args.genre)
         for d_idx, dial in enumerate(dials):
             print("Dialog %d with %d phrases:" % (d_idx, len(dial)))
@@ -30,13 +31,13 @@ if __name__ == "__main__":
                 print(" ".join(p))
             print()
 
-        phrase_pairs, emb_dict = data.load_data(genre_filter=args.data)
-        rev_emb_dict = {idx: word for word, idx in emb_dict.items()}
-        words_stat = collections.Counter()
+    phrase_pairs, emb_dict = data.load_data(genre_filter=args.genre)
+    rev_emb_dict = {idx: word for word, idx in emb_dict.items()}
+    words_stat = collections.Counter()
 
-        for p1, p2 in phrase_pairs:
-            words_stat.update(p1)
-        print("Frequency stats for %d tokens in the dict" % len(emb_dict))
-        for token, count in words_stat.most_common():
-            print("%s: %d" % (rev_emb_dict[token], count))
+    for p1, p2 in phrase_pairs:
+        words_stat.update(p1)
+    print("Frequency stats for %d tokens in the dict" % len(emb_dict))
+    for token, count in words_stat.most_common():
+        print("%s: %d" % (rev_emb_dict[token], count))
     pass
