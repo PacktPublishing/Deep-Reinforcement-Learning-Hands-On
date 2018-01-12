8c223b27d5b1c188521d6376fbd779ccb738c2fb
Max Lapan <max.lapan@gmail.com>
2018-01-12 16:36:09 +0100

    Another fix

--- ch12/telergam_bot.py
+++ ch12/telergam_bot.py
@@ -31,28 +31,28 @@ if __name__ == "__main__":
                         help="Configuration file for the bot, default=" + CONFIG_DEFAULT)
     parser.add_argument("-m", "--model", required=True, help="Model to load")
     parser.add_argument("--sample", default=False, action='store_true', help="Enable sampling mode")
-    args = parser.parse_args()
+    prog_args = parser.parse_args()
 
     conf = configparser.ConfigParser()
-    if not conf.read(os.path.expanduser(args.config)):
-        log.error("Configuration file %s not found", args.config)
+    if not conf.read(os.path.expanduser(prog_args.config)):
+        log.error("Configuration file %s not found", prog_args.config)
         sys.exit()
 
-    emb_dict = data.load_emb_dict(os.path.dirname(args.model))
+    emb_dict = data.load_emb_dict(os.path.dirname(prog_args.model))
     log.info("Loaded embedded dict with %d entries", len(emb_dict))
     rev_emb_dict = {idx: word for word, idx in emb_dict.items()}
     end_token = emb_dict[data.END_TOKEN]
 
     net = model.PhraseModel(emb_size=model.EMBEDDING_DIM, dict_size=len(emb_dict), hid_size=model.HIDDEN_STATE_SIZE)
-    net.load_state_dict(torch.load(args.model))
+    net.load_state_dict(torch.load(prog_args.model))
 
-    def bot_func(bot, update, bot_args):
-        text = " ".join(bot_args)
+    def bot_func(bot, update, args):
+        text = " ".join(args)
         words = utils.tokenize(text)
         seq_1 = data.encode_words(words, emb_dict)
         input_seq = model.pack_input(seq_1, net.emb)
         enc = net.encode(input_seq)
-        if args.sample:
+        if prog_args.sample:
             _, tokens = net.decode_chain_sampling(net.emb, enc, input_seq.data[0:1],
                                                 seq_len=data.MAX_TOKENS, stop_at_token=end_token)
         else:
