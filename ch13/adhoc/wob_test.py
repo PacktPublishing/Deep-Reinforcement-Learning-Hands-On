076b3308d4b7bab8a8488dd79d12a2fe17fbab45
Max Lapan <max.lapan@gmail.com>
2018-01-18 13:03:26 +0100

    WoB observation wrapper

--- ch13/adhoc/wob_test.py
+++ ch13/adhoc/wob_test.py
@@ -1,21 +1,41 @@
+import sys
+sys.path.append("..")
+import time
 import gym
+# this import is not being used, but is required to make Universe envs available!
 import universe
 
+from PIL import Image
 
-def configure(env):
-    e = env
-    while True:
-        if e.metadata.get('configure.required') and hasattr(e, '_configure') and not isinstance(e, gym.Wrapper):
-            e._configure()
-            print("Configured!")
-            break
-        e = e.env
+from lib import wob_vnc
+
 
 if __name__ == "__main__":
     env = gym.make("wob.mini.BisectAngle-v0")
-    configure(env)
+    env = wob_vnc.MiniWoBCropper(env)
+    env.configure(remotes=1)
     print(env)
-##    obs = env.reset()
-#    print(obs)
+    obs = env.reset()
+    saved = False
+
+    while True:
+        time.sleep(1)
+        a = env.action_space.sample()
+        obs, reward, is_done, info = env.step([a])
+        if obs[0] is None:
+            print("Env is still resetting...")
+            continue
+        print("Sampled action: ", a)
+        print("Response are:")
+        print("Observation", obs[0].shape)
+        print("Reward", reward)
+        print("Is done", is_done)
+        print("Info", info)
+
+        if not saved:
+            im = Image.fromarray(obs[0])
+            im.save("image-cropped.png")
+            saved = True
+
     env.close()
     pass
---
+++ ch13/lib/wob_vnc.py
@@ -0,0 +1,19 @@
+import gym
+
+# Area of interest
+WIDTH = 160
+HEIGHT = 210
+X_OFS = 10
+Y_OFS = 75
+
+
+class MiniWoBCropper(gym.ObservationWrapper):
+    def _observation(self, observation_n):
+        res = []
+        for obs in observation_n:
+            if obs is None:
+                res.append(obs)
+                continue
+            res.append(obs['vision'][Y_OFS:Y_OFS+HEIGHT, X_OFS:X_OFS+WIDTH, :])
+        return res
+
