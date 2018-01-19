51bda3a7eb3adf976b76f91e84bec6747a111072
Max Lapan <max.lapan@gmail.com>
2018-01-19 15:09:38 +0100

    Training

---
+++ ch13/lib/common.py
@@ -0,0 +1,79 @@
+import sys
+import time
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+
+class RewardTracker:
+    def __init__(self, writer):
+        self.writer = writer
+
+    def __enter__(self):
+        self.ts = time.time()
+        self.ts_frame = 0
+        self.total_rewards = []
+        return self
+
+    def __exit__(self, *args):
+        self.writer.close()
+
+    def reward(self, reward, frame, epsilon=None):
+        self.total_rewards.append(reward)
+        speed = (frame - self.ts_frame) / (time.time() - self.ts)
+        self.ts_frame = frame
+        self.ts = time.time()
+        mean_reward = np.mean(self.total_rewards[-100:])
+        epsilon_str = "" if epsilon is None else ", eps %.2f" % epsilon
+        print("%d: done %d games, mean reward %.3f, speed %.2f f/s%s" % (
+            frame, len(self.total_rewards), mean_reward, speed, epsilon_str
+        ))
+        sys.stdout.flush()
+        if epsilon is not None:
+            self.writer.add_scalar("epsilon", epsilon, frame)
+        self.writer.add_scalar("speed", speed, frame)
+        self.writer.add_scalar("reward_100", mean_reward, frame)
+        self.writer.add_scalar("reward", reward, frame)
+
+
+def unpack_batch(batch, net, last_val_gamma, cuda=False):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = Variable(torch.from_numpy(np.array(states, copy=False)))
+    actions_t = torch.LongTensor(actions)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_t = actions_t.cuda()
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = Variable(torch.from_numpy(np.array(last_states, copy=False)), volatile=True)
+        if cuda:
+            last_states_v = last_states_v.cuda()
+        last_vals_v = net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += last_val_gamma * last_vals_np
+
+    ref_vals_v = Variable(torch.from_numpy(rewards_np))
+    if cuda:
+        ref_vals_v = ref_vals_v.cuda()
+
+    return states_v, actions_t, ref_vals_v
--- ch13/wob_click_train.py
+++ ch13/wob_click_train.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 import gym
-import time
 import universe
 import argparse
-import numpy as np
+from tensorboardX import SummaryWriter
 
-from lib import wob_vnc, model_vnc
+from lib import wob_vnc, model_vnc, common
 
 import ptan
+
 import torch
-from torch.autograd import Variable
+import torch.nn.utils as nn_utils
+import torch.nn.functional as F
+import torch.optim as optim
+
 
 ENV_NAME = "wob.mini.BisectAngle-v0"
 #REMOTE_ADDR = 'vnc://gpu:5900+15900'
@@ -20,10 +23,12 @@ REMOTE_ADDR = 'vnc://gpu:5900+15900,gpu:5901+15901'
 # docker run -d -p 5900:5900 -p 15900:15900 --privileged --ipc host --cap-add SYS_ADMIN quay.io/openai/universe.world-of-bits:0.20.0
 # docker run -d -p 5901:5900 -p 15901:15900 --privileged --ipc host --cap-add SYS_ADMIN quay.io/openai/universe.world-of-bits:0.20.0
 
-
 GAMMA = 0.99
 REWARD_STEPS = 4
 BATCH_SIZE = 32
+LEARNING_RATE = 0.001
+ENTROPY_BETA = 0.01
+CLIP_GRAD = 0.1
 
 
 def step_env(env, action):
@@ -41,6 +46,8 @@ if __name__ == "__main__":
     parser.add_argument("--cuda", default=False, action='store_true', help="CUDA mode")
     args = parser.parse_args()
 
+    writer = SummaryWriter(comment="-wob_click_" + ENV_NAME.split('.')[-1] + "_" + args.name)
+
     env = gym.make(ENV_NAME)
     env = universe.wrappers.experimental.SoftmaxClickMouse(env)
     env = wob_vnc.MiniWoBCropper(env)
@@ -49,24 +56,55 @@ if __name__ == "__main__":
 
     net = model_vnc.Model(input_shape=(3, wob_vnc.HEIGHT, wob_vnc.WIDTH),
                           n_actions=env.action_space.n)
+    if args.cuda:
+        net.cuda()
     print(net)
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
 
     agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], cuda=args.cuda,
                                    apply_softmax=True)
     exp_source = ptan.experience.ExperienceSourceFirstLast(
         [env], agent, gamma=GAMMA, steps_count=REWARD_STEPS, vectorized=True)
 
-    # obs, reward, done, info = step_env(env, env.action_space.sample())
-    # obs_v = Variable(torch.from_numpy(np.array(obs)))
-    # r = net(obs_v)
-    # print(r[0].size(), r[1].size())
+    with common.RewardTracker(writer) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            batch = []
+            for step_idx, exp in enumerate(exp_source):
+                for reward in exp_source.pop_total_rewards():
+                    tracker.reward(reward, step_idx)
+                batch.append(exp)
+                if len(batch) < BATCH_SIZE:
+                    continue
 
-    batch = []
-    for idx, exp in enumerate(exp_source):
-        batch.append(exp)
-        if len(batch) < BATCH_SIZE:
-            continue
+                states_v, actions_t, vals_ref_v = \
+                    common.unpack_batch(batch, net, last_val_gamma=GAMMA ** REWARD_STEPS,
+                                        cuda=args.cuda)
+                batch.clear()
 
-        break
+                optimizer.zero_grad()
+                logits_v, value_v = net(states_v)
+
+                loss_value_v = F.mse_loss(value_v, vals_ref_v)
+
+                log_prob_v = F.log_softmax(logits_v)
+                adv_v = vals_ref_v - value_v.detach()
+                log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                loss_policy_v = -log_prob_actions_v.mean()
+
+                prob_v = F.softmax(logits_v)
+                entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+
+                loss_v = entropy_loss_v + loss_value_v + loss_policy_v
+                loss_v.backward()
+                nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+                optimizer.step()
+
+                tb_tracker.track("advantage", adv_v, step_idx)
+                tb_tracker.track("values", value_v, step_idx)
+                tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+                tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+                tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+                tb_tracker.track("loss_value", loss_value_v, step_idx)
+                tb_tracker.track("loss_total", loss_v, step_idx)
 
     pass
