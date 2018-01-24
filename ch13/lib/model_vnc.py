c332fa7655dcc8ab2e8bf0e792643d2a295818b1
Max Lapan <max.lapan@gmail.com>
2018-01-24 15:28:55 +0100

    Extra layer in val net

--- ch13/lib/model_vnc.py
+++ ch13/lib/model_vnc.py
@@ -67,7 +67,9 @@ class ModelMultimodal(nn.Module):
         )
 
         self.value = nn.Sequential(
-            nn.Linear(conv_out_size + MM_HIDDEN_SIZE*2, 1),
+            nn.Linear(conv_out_size + MM_HIDDEN_SIZE*2, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
         )
 
     def _get_conv_out(self, shape):
