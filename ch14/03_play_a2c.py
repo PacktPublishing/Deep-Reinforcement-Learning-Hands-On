fef8c39452a659d9e34fbf88e12706d47926e586
Max Lapan <max.lapan@gmail.com>
2018-02-03 12:30:43 +0100

    Play tool

---
+++ ch14/03_play_a2c.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import argparse
+import gym
+import pybullet_envs
+
+from lib import model
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+
+ENV_ID = "MinitaurBulletEnv-v0"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", required=True, help="Model file to load")
+    parser.add_argument("-e", "--env", default=ENV_ID, help="Environment name to use, default=" + ENV_ID)
+    parser.add_argument("-r", "--record", help="If specified, sets the recording dir, default=Disabled")
+    args = parser.parse_args()
+
+    # durty hack to envable visualisation
+    spec = gym.envs.registry.spec(args.env)
+    spec._kwargs['render'] = False
+    env = gym.make(args.env)
+    if args.record:
+        env = gym.wrappers.Monitor(env, args.record)
+
+    net = model.Model(env.observation_space.shape[0], env.action_space.shape[0])
+    net.load_state_dict(torch.load(args.model))
+
+    obs = env.reset()
+    total_reward = 0.0
+    total_steps = 0
+    while True:
+        obs_v = Variable(torch.from_numpy(np.array([obs], dtype=np.float32)))
+        mu_v, var_v, val_v = net(obs_v)
+        action = mu_v.squeeze(dim=0).data.numpy()
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+        if done:
+            break
+    print("In %d steps we got %.3f reward" % (total_steps, total_reward))
