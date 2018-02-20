4e1914863aa599252302213b57c14e5f96f124f1
Max Lapan <max.lapan@gmail.com>
2018-02-20 20:21:14 +0100

    Forgotten loop :)

--- ch16/02_hyper.py
+++ ch16/02_hyper.py
@@ -2,19 +2,19 @@
 import subprocess
 import random
 
-LEARNING_RATE_EXP = (-5, -1)
+LEARNING_RATE_EXP = (-5, -2)
 NOISE_STD_EXP = (-5, -0.5)
 ITERS = 20
 
 if __name__ == "__main__":
-    random.seed(1234)
-    lr_exp = random.uniform(LEARNING_RATE_EXP[0], LEARNING_RATE_EXP[1])
-    std_exp = random.uniform(NOISE_STD_EXP[0], NOISE_STD_EXP[1])
-    lr = 10 ** lr_exp
-    std = 10 ** std_exp
-    print(lr_exp, std_exp)
-    print(lr, std)
-    res = subprocess.run(["./02_breakout_es.py", "--cuda", "--lr", str(lr), '--noise-std', str(std),
-                          "--iters", str(ITERS)])
-    print(res)
+    random.seed(1235)
+    while True:
+        lr_exp = random.uniform(LEARNING_RATE_EXP[0], LEARNING_RATE_EXP[1])
+        std_exp = random.uniform(NOISE_STD_EXP[0], NOISE_STD_EXP[1])
+        lr = 10 ** lr_exp
+        std = 10 ** std_exp
+        print(lr_exp, std_exp)
+        print(lr, std)
+        res = subprocess.run(["./02_breakout_es.py", "--cuda", "--lr", str(lr), '--noise-std', str(std),
+                              "--iters", str(ITERS)])
     pass
