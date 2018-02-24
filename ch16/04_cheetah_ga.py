3af8ecdf0079e4a4f2f3881bec1fbdc592f9876e
Max Lapan <max.lapan@gmail.com>
2018-02-24 20:00:39 +0100

    GA parallel cheetah

--- ch16/04_cheetah_ga.py
+++ ch16/04_cheetah_ga.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
+import sys
 import gym
 import roboschool
+import collections
 import copy
+import time
 import numpy as np
 
 import torch
 import torch.nn as nn
+import torch.multiprocessing as mp
 from torch.autograd import Variable
 
 from tensorboardX import SummaryWriter
 
+
 NOISE_STD = 0.005
 POPULATION_SIZE = 2000
-PARENTS_COUNT = 200
+PARENTS_COUNT = 10
+WORKERS_COUNT = 6
+SEEDS_PER_WORKER = POPULATION_SIZE // WORKERS_COUNT
 
 
 class Net(nn.Module):
@@ -35,38 +42,89 @@ class Net(nn.Module):
 def evaluate(env, net):
     obs = env.reset()
     reward = 0.0
+    steps = 0
     while True:
         obs_v = Variable(torch.from_numpy(np.array([obs], dtype=np.float32)), volatile=True)
         action_v = net(obs_v)
         obs, r, done, _ = env.step(action_v.data.numpy()[0])
         reward += r
+        steps += 1
         if done:
             break
-    return reward
+    return reward, steps
 
 
-def mutate_parent(net):
-    new_net = copy.deepcopy(net)
+def mutate_net(net, seed, copy_net=True):
+    new_net = copy.deepcopy(net) if copy_net else net
+    np.random.seed(seed)
     for p in new_net.parameters():
         noise_t = torch.from_numpy(np.random.normal(size=p.data.size()).astype(np.float32))
         p.data += NOISE_STD * noise_t
     return new_net
 
 
-if __name__ == "__main__":
-    writer = SummaryWriter(comment="-cheetah-ga")
+def build_net(env, seeds):
+    torch.manual_seed(seeds[0])
+    net = Net(env.observation_space.shape[0], env.action_space.shape[0])
+    for seed in seeds[1:]:
+        net = mutate_net(net, seed, copy_net=False)
+    return net
+
+
+OutputItem = collections.namedtuple('OutputItem', field_names=['seeds', 'reward', 'steps'])
+
+
+def worker_func(input_queue, output_queue):
     env = gym.make("RoboschoolHalfCheetah-v1")
+    cache = {}
+
+    while True:
+        parents = input_queue.get()
+        if parents is None:
+            break
+        new_cache = {}
+        for net_seeds in parents:
+            if len(net_seeds) > 1:
+                net = cache.get(net_seeds[:-1])
+                if net is not None:
+                    net = mutate_net(net, net_seeds[-1])
+                else:
+                    net = build_net(env, net_seeds)
+            else:
+                net = build_net(env, net_seeds)
+            new_cache[net_seeds] = net
+            reward, steps = evaluate(env, net)
+            output_queue.put(OutputItem(seeds=net_seeds, reward=reward, steps=steps))
+        cache = new_cache
+
+
+if __name__ == "__main__":
+    mp.set_start_method('spawn')
+    writer = SummaryWriter(comment="-cheetah-ga")
+
+    input_queues = []
+    output_queue = mp.Queue(maxsize=WORKERS_COUNT)
+    workers = []
+    for _ in range(WORKERS_COUNT):
+        input_queue = mp.Queue(maxsize=1)
+        input_queues.append(input_queue)
+        w = mp.Process(target=worker_func, args=(input_queue, output_queue))
+        w.start()
+        seeds = [(np.random.randint(65536),) for _ in range(SEEDS_PER_WORKER)]
+        input_queue.put(seeds)
 
     gen_idx = 0
-    nets = [
-        Net(env.observation_space.shape[0], env.action_space.shape[0])
-        for _ in range(POPULATION_SIZE)
-    ]
-    population = [
-        (net, evaluate(env, net))
-        for net in nets
-    ]
+    elite = None
     while True:
+        t_start = time.time()
+        batch_steps = 0
+        population = []
+        while len(population) < SEEDS_PER_WORKER * WORKERS_COUNT:
+            out_item = output_queue.get()
+            population.append((out_item.seeds, out_item.reward))
+            batch_steps += out_item.steps
+        if elite is not None:
+            population.append(elite)
         population.sort(key=lambda p: p[1], reverse=True)
         rewards = [p[1] for p in population[:PARENTS_COUNT]]
         reward_mean = np.mean(rewards)
@@ -75,18 +133,20 @@ if __name__ == "__main__":
         writer.add_scalar("reward_mean", reward_mean, gen_idx)
         writer.add_scalar("reward_std", reward_std, gen_idx)
         writer.add_scalar("reward_max", reward_max, gen_idx)
-        print("%d: reward_mean=%.2f, reward_max=%.2f, reward_std=%.2f" % (
-            gen_idx, reward_mean, reward_max, reward_std))
+        writer.add_scalar("batch_steps", batch_steps, gen_idx)
+        speed = batch_steps / (time.time() - t_start)
+        writer.add_scalar("speed", speed, gen_idx)
+        print("%d: reward_mean=%.2f, reward_max=%.2f, reward_std=%.2f, speed=%.2f f/s" % (
+            gen_idx, reward_mean, reward_max, reward_std, speed))
 
-        # generate next population
-        prev_population = population
-        population = [population[0]]
-        for _ in range(POPULATION_SIZE-1):
-            parent_idx = np.random.randint(0, PARENTS_COUNT)
-            parent = prev_population[parent_idx][0]
-            net = mutate_parent(parent)
-            fitness = evaluate(env, net)
-            population.append((net, fitness))
+        elite = population[0]
+        for worker_queue in input_queues:
+            seeds = []
+            for _ in range(SEEDS_PER_WORKER):
+                parent = np.random.randint(PARENTS_COUNT)
+                next_seed = np.random.randint(65536)
+                seeds.append(tuple(list(population[parent][0]) + [next_seed]))
+            worker_queue.put(seeds)
         gen_idx += 1
 
     pass
