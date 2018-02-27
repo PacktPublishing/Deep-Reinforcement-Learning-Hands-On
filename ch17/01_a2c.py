e46c1ac01f7a625192dcb113f0d7b8cf4d4d35da
Max Lapan <max.lapan@gmail.com>
2018-02-27 10:59:00 +0100

    Breakout A2C

---
+++ ch17/01_a2c.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+import gym
+import ptan
+import numpy as np
+import argparse
+from tensorboardX import SummaryWriter
+
+import torch
+import torch.nn as nn
+import torch.nn.utils as nn_utils
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+
+GAMMA = 0.99
+LEARNING_RATE = 0.001
+ENTROPY_BETA = 0.01
+BATCH_SIZE = 128
+NUM_ENVS = 50
+
+REWARD_BOUND = 400
+REWARD_STEPS = 5
+CLIP_GRAD = 0.1
+
+
+class AtariA2C(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(AtariA2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, padding=1),
+            nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, padding=1),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
+
+def unpack_batch(batch, net, cuda=False):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = Variable(torch.from_numpy(np.array(states, copy=False)))
+    actions_t = torch.LongTensor(actions)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_t = actions_t.cuda()
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = Variable(torch.from_numpy(np.array(last_states, copy=False)), volatile=True)
+        if cuda:
+            last_states_v = last_states_v.cuda()
+        last_vals_v = net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
+
+    ref_vals_v = Variable(torch.from_numpy(rewards_np))
+    if cuda:
+        ref_vals_v = ref_vals_v.cuda()
+
+    return states_v, actions_t, ref_vals_v
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("-n", "--name", required=True, help="Name of the run")
+    args = parser.parse_args()
+
+    make_env = lambda: ptan.common.wrappers.wrap_dqn(gym.make("BreakoutNoFrameskip-v4"))
+    envs = [make_env() for _ in range(NUM_ENVS)]
+    writer = SummaryWriter(comment="-breakout-a2c_" + args.name)
+
+    net = AtariA2C(envs[0].observation_space.shape, envs[0].action_space.n)
+    if args.cuda:
+        net.cuda()
+    print(net)
+
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], apply_softmax=True, cuda=args.cuda)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
+
+    optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
+
+    batch = []
+
+    with ptan.common.utils.RewardTracker(writer) as tracker:
+        with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
+            for step_idx, exp in enumerate(exp_source):
+                batch.append(exp)
+
+                # handle new rewards
+                new_rewards = exp_source.pop_total_rewards()
+                if new_rewards:
+                    mean_reward = tracker.reward(new_rewards[0], step_idx)
+                    if mean_reward is not None and mean_reward > REWARD_BOUND:
+                        print("Solved in %d steps" % step_idx)
+                        break
+
+                if len(batch) < BATCH_SIZE:
+                    continue
+
+                states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=args.cuda)
+                batch.clear()
+
+                optimizer.zero_grad()
+                logits_v, value_v = net(states_v)
+
+                loss_value_v = F.mse_loss(value_v, vals_ref_v)
+
+                log_prob_v = F.log_softmax(logits_v)
+                adv_v = vals_ref_v - value_v.detach()
+                log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+                loss_policy_v = -log_prob_actions_v.mean()
+
+                prob_v = F.softmax(logits_v)
+                entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+
+                # calculate policy gradients only
+                loss_policy_v.backward(retain_graph=True)
+                grads = np.concatenate([p.grad.data.cpu().numpy().flatten()
+                                        for p in net.parameters()
+                                        if p.grad is not None])
+
+                # apply entropy and value gradients
+                loss_v = entropy_loss_v + loss_value_v
+                loss_v.backward()
+                nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+                optimizer.step()
+                # get full loss
+                loss_v += loss_policy_v
+
+                tb_tracker.track("advantage",       adv_v, step_idx)
+                tb_tracker.track("values",          value_v, step_idx)
+                tb_tracker.track("batch_rewards",   vals_ref_v, step_idx)
+                tb_tracker.track("loss_entropy",    entropy_loss_v, step_idx)
+                tb_tracker.track("loss_policy",     loss_policy_v, step_idx)
+                tb_tracker.track("loss_value",      loss_value_v, step_idx)
+                tb_tracker.track("loss_total",      loss_v, step_idx)
+                tb_tracker.track("grad_l2",         np.sqrt(np.mean(np.square(grads))), step_idx)
+                tb_tracker.track("grad_max",        np.max(np.abs(grads)), step_idx)
+                tb_tracker.track("grad_var",        np.var(grads), step_idx)
