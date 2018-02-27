4469d3b3a792531a87c2ba9cafb2fe78293c250c
Max Lapan <max.lapan@gmail.com>
2018-02-27 19:59:37 +0100

    Cleanups

--- ch17/lib/common.py
+++ ch17/lib/common.py
@@ -112,19 +112,10 @@ def train_on_batch(step_idx, net, optimizer, tb_tracker,
     prob_v = F.softmax(logits_v)
     entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
 
-    # calculate policy gradients only
-    loss_policy_v.backward(retain_graph=True)
-    grads = np.concatenate([p.grad.data.cpu().numpy().flatten()
-                            for p in net.parameters()
-                            if p.grad is not None])
-
-    # apply entropy and value gradients
-    loss_v = entropy_loss_v + loss_value_v
+    loss_v = entropy_loss_v + loss_value_v + loss_policy_v
     loss_v.backward()
     nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
     optimizer.step()
-    # get full loss
-    loss_v += loss_policy_v
 
     tb_tracker.track("advantage", adv_v, step_idx)
     tb_tracker.track("values", value_v, step_idx)
@@ -133,8 +124,5 @@ def train_on_batch(step_idx, net, optimizer, tb_tracker,
     tb_tracker.track("loss_policy", loss_policy_v, step_idx)
     tb_tracker.track("loss_value", loss_value_v, step_idx)
     tb_tracker.track("loss_total", loss_v, step_idx)
-    tb_tracker.track("grad_l2", np.sqrt(np.mean(np.square(grads))), step_idx)
-    tb_tracker.track("grad_max", np.max(np.abs(grads)), step_idx)
-    tb_tracker.track("grad_var", np.var(grads), step_idx)
 
     return logits_v
