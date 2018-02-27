efdaa73d4a3b23deb8baba80137f4f7cb6f969b1
Max Lapan <max.lapan@gmail.com>
2018-02-27 14:31:34 +0100

    Refactor

---
+++ ch17/lib/common.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils as nn_utils
+from torch.autograd import Variable
+
+GAMMA = 0.99
+REWARD_STEPS = 5
+ENTROPY_BETA = 0.01
+BATCH_SIZE = 128
+CLIP_GRAD = 0.1
+
+
+class AtariA2C(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(AtariA2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, padding=1),
+            nn.Conv2d(32, 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, padding=1),
+            nn.Conv2d(32, 64, kernel_size=4, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(Variable(torch.zeros(1, *shape)))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
+
+def unpack_batch(batch, net, cuda=False):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(np.array(exp.state, copy=False))
+        actions.append(int(exp.action))
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = Variable(torch.from_numpy(np.array(states, copy=False)))
+    actions_t = torch.LongTensor(actions)
+    if cuda:
+        states_v = states_v.cuda()
+        actions_t = actions_t.cuda()
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = Variable(torch.from_numpy(np.array(last_states, copy=False)), volatile=True)
+        if cuda:
+            last_states_v = last_states_v.cuda()
+        last_vals_v = net(last_states_v)[1]
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
+
+    ref_vals_v = Variable(torch.from_numpy(rewards_np))
+    if cuda:
+        ref_vals_v = ref_vals_v.cuda()
+
+    return states_v, actions_t, ref_vals_v
+
+
+def train_on_batch(step_idx, batch, net, optimizer, tb_tracker, cuda=False):
+    assert isinstance(batch, list)
+    assert len(batch) == BATCH_SIZE
+
+    states_v, actions_t, vals_ref_v = unpack_batch(batch, net, cuda=cuda)
+
+    optimizer.zero_grad()
+    logits_v, value_v = net(states_v)
+
+    loss_value_v = F.mse_loss(value_v, vals_ref_v)
+
+    log_prob_v = F.log_softmax(logits_v)
+    adv_v = vals_ref_v - value_v.detach()
+    log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
+    loss_policy_v = -log_prob_actions_v.mean()
+
+    prob_v = F.softmax(logits_v)
+    entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
+
+    # calculate policy gradients only
+    loss_policy_v.backward(retain_graph=True)
+    grads = np.concatenate([p.grad.data.cpu().numpy().flatten()
+                            for p in net.parameters()
+                            if p.grad is not None])
+
+    # apply entropy and value gradients
+    loss_v = entropy_loss_v + loss_value_v
+    loss_v.backward()
+    nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+    optimizer.step()
+    # get full loss
+    loss_v += loss_policy_v
+
+    tb_tracker.track("advantage", adv_v, step_idx)
+    tb_tracker.track("values", value_v, step_idx)
+    tb_tracker.track("batch_rewards", vals_ref_v, step_idx)
+    tb_tracker.track("loss_entropy", entropy_loss_v, step_idx)
+    tb_tracker.track("loss_policy", loss_policy_v, step_idx)
+    tb_tracker.track("loss_value", loss_value_v, step_idx)
+    tb_tracker.track("loss_total", loss_v, step_idx)
+    tb_tracker.track("grad_l2", np.sqrt(np.mean(np.square(grads))), step_idx)
+    tb_tracker.track("grad_max", np.max(np.abs(grads)), step_idx)
+    tb_tracker.track("grad_var", np.var(grads), step_idx)
