fe4d0bfa8643857bebc8f2cf6301837256089f49
Max Lapan <max.lapan@gmail.com>
2018-02-28 12:14:09 +0100

    Reporting

--- ch17/01_a2c.py
+++ ch17/01_a2c.py
@@ -33,6 +33,8 @@ def iterate_train_batches(envs, net, cuda=False):
     act_selector = ptan.actions.ProbabilityActionSelector()
     obs = [e.reset() for e in envs]
     cur_dones = [False] * NUM_ENVS
+    total_reward = [0.0] * NUM_ENVS
+    total_steps = [0] * NUM_ENVS
     mb_obs = np.zeros((NUM_ENVS, common.REWARD_STEPS) + common.IMG_SHAPE, dtype=np.uint8)
     mb_rewards = np.zeros((NUM_ENVS, common.REWARD_STEPS), dtype=np.float32)
     mb_values = np.zeros((NUM_ENVS, common.REWARD_STEPS), dtype=np.float32)
@@ -40,6 +42,8 @@ def iterate_train_batches(envs, net, cuda=False):
     mb_actions = np.zeros((NUM_ENVS, common.REWARD_STEPS), dtype=np.int32)
 
     while True:
+        done_rewards = []
+        done_steps = []
         for n in range(common.REWARD_STEPS):
             obs_v = ptan.agent.default_states_preprocessor(obs)
             mb_obs[:, n] = obs_v.data.numpy()
@@ -53,8 +57,14 @@ def iterate_train_batches(envs, net, cuda=False):
             mb_values[:, n] = values_v.squeeze().data.cpu().numpy()
             for e_idx, e in enumerate(envs):
                 o, r, done, _ = e.step(actions[e_idx])
+                total_reward[e_idx] += r
+                total_steps[e_idx] += 1
                 if done:
                     o = e.reset()
+                    done_rewards.append(total_reward[e_idx])
+                    done_steps.append(total_steps[e_idx])
+                    total_reward[e_idx] = 0.0
+                    total_steps[e_idx] = 0
                 obs[e_idx] = o
                 mb_rewards[e_idx, n] = r
                 cur_dones[e_idx] = done
@@ -79,7 +89,7 @@ def iterate_train_batches(envs, net, cuda=False):
         out_mb_rewards = mb_rewards.flatten()
         out_mb_actions = mb_actions.flatten()
         out_mb_values = mb_values.flatten()
-        yield out_mb_obs, out_mb_rewards, out_mb_actions, out_mb_values
+        yield out_mb_obs, out_mb_rewards, out_mb_actions, out_mb_values, np.array(done_rewards), np.array(done_steps)
 
 
 
@@ -100,8 +110,20 @@ if __name__ == "__main__":
     optimizer = optim.RMSprop(net.parameters(), lr=LEARNING_RATE, eps=1e-5)
 
     step_idx = 0
+    best_reward = None
     with ptan.common.utils.TBMeanTracker(writer, batch_size=10) as tb_tracker:
-        for mb_obs, mb_rewards, mb_actions, mb_values in iterate_train_batches(envs, net, cuda=args.cuda):
+        for mb_obs, mb_rewards, mb_actions, mb_values, done_rewards, done_steps in iterate_train_batches(envs, net, cuda=args.cuda):
+            if len(done_rewards) > 0:
+                if best_reward is None:
+                    best_reward = done_rewards.max()
+                elif best_reward < done_rewards.max():
+                    best_reward = done_rewards.max()
+                tb_tracker.track("total_reward_max", best_reward, step_idx)
+                tb_tracker.track("total_reward", done_rewards, step_idx)
+                tb_tracker.track("total_steps", done_steps, step_idx)
+                print("%d: done %d episodes, mean_reward=%.2f, max_reward=%.2f" % (
+                    step_idx, len(done_rewards), done_rewards.mean(), done_rewards.max()))
+
             optimizer.zero_grad()
             mb_adv = mb_rewards - mb_values
             adv_v = Variable(torch.from_numpy(mb_adv))
