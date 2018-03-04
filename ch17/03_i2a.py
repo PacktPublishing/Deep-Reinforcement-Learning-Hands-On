Fix broadcastable

ch17/03_i2a.py
@@ -10,6 +10,7 @@ from lib import common, i2a
 import torch.optim as optim
 import torch
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 from tensorboardX import SummaryWriter
 
@@ -91,7 +92,7 @@ if __name__ == "__main__":
             obs_v = common.train_a2c(net_i2a, mb_obs, mb_rewards, mb_actions, mb_values,
                                      optimizer, tb_tracker, step_idx, cuda=args.cuda)
             # policy distillation
-            probs_v = torch.from_numpy(mb_probs)
+            probs_v = Variable(torch.from_numpy(mb_probs))
             if args.cuda:
                 probs_v = probs_v.cuda()
             policy_opt.zero_grad()

ch17/lib/common.py
@@ -88,6 +88,7 @@ def discount_with_dones(rewards, dones, gamma):
 
 
 def iterate_batches(envs, net, cuda=False):
+    n_actions = envs[0].action_space.n
     act_selector = ptan.actions.ProbabilityActionSelector()
     obs = [e.reset() for e in envs]
     batch_dones = [[False] for _ in range(NUM_ENVS)]
@@ -97,7 +98,7 @@ def iterate_batches(envs, net, cuda=False):
     mb_rewards = np.zeros((NUM_ENVS, REWARD_STEPS), dtype=np.float32)
     mb_values = np.zeros((NUM_ENVS, REWARD_STEPS), dtype=np.float32)
     mb_actions = np.zeros((NUM_ENVS, REWARD_STEPS), dtype=np.int32)
-    mb_probs = np.zeros((NUM_ENVS, REWARD_STEPS), dtype=np.float32)
+    mb_probs = np.zeros((NUM_ENVS, REWARD_STEPS, n_actions), dtype=np.float32)
 
     while True:
         batch_dones = [[dones[-1]] for dones in batch_dones]