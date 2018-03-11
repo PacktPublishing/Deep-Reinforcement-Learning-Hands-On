db53992bd66b10149bad988da9e5edfa7632427b
Max Lapan <max.lapan@gmail.com>
2018-03-11 21:14:49 +0100

    Show steps

--- ch18/train.py
+++ ch18/train.py
@@ -100,8 +100,8 @@ if __name__ == "__main__":
             speed_nodes = game_nodes / dt
             tb_tracker.track("speed_steps", speed_steps, step_idx)
             tb_tracker.track("speed_nodes", speed_nodes, step_idx)
-            print("Game %d, score %4.1f, mean %.2f, steps/s %.2f, nodes/s %.2f" % (
-                step_idx, game_res, mean_score, speed_steps, speed_nodes))
+            print("Game %d, steps %d, score %4.1f, mean %.2f, steps/s %.2f, nodes/s %.2f" % (
+                step_idx, game_steps, game_res, mean_score, speed_steps, speed_nodes))
 
             if len(replay_buffer) < MIN_REPLAY_TO_TRAIN:
                 continue
