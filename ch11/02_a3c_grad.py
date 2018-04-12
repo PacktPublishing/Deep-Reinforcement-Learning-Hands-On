Fix softmax

ch11/02_a3c_grad.py
@@ -72,12 +72,12 @@ def grads_func(proc_name, net, cuda, train_queue):
                 logits_v, value_v = net(states_v)
                 loss_value_v = F.mse_loss(value_v, vals_ref_v)
 
-                log_prob_v = F.log_softmax(logits_v)
+                log_prob_v = F.log_softmax(logits_v, dim=1)
                 adv_v = vals_ref_v - value_v.detach()
                 log_prob_actions_v = adv_v * log_prob_v[range(GRAD_BATCH), actions_t]
                 loss_policy_v = -log_prob_actions_v.mean()
 
-                prob_v = F.softmax(logits_v)
+                prob_v = F.softmax(logits_v, dim=1)
                 entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
 
                 loss_v = entropy_loss_v + loss_value_v + loss_policy_v