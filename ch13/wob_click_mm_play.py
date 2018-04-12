Fix softmax

ch13/wob_click_mm_play.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
                 args.name, round_idx, step_idx, reward, idle_count, int(done))
             obs_v = preprocessor([obs])
             logits_v = net(obs_v)[0]
-            policy = F.softmax(logits_v).data.numpy()[0]
+            policy = F.softmax(logits_v, dim=1).data.numpy()[0]
             action = np.random.choice(len(policy), p=policy)
             wob_vnc.save_obs(obs[0], img_name, action=action)
             step_idx += 1

ch13/wob_click_play.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
                 print(step_idx, reward, done, idle_count, info)
             obs_v = Variable(torch.from_numpy(np.array([obs])))
             logits_v = net(obs_v)[0]
-            policy = F.softmax(logits_v).data.numpy()[0]
+            policy = F.softmax(logits_v, dim=1).data.numpy()[0]
             action = np.random.choice(len(policy), p=policy)
             step_idx += 1
             reward_sum += reward

ch13/wob_click_train.py
@@ -119,12 +119,12 @@ if __name__ == "__main__":
 
                 loss_value_v = F.mse_loss(value_v, vals_ref_v)
 
-                log_prob_v = F.log_softmax(logits_v)
+                log_prob_v = F.log_softmax(logits_v, dim=1)
                 adv_v = vals_ref_v - value_v.detach()
                 log_prob_actions_v = adv_v * log_prob_v[range(BATCH_SIZE), actions_t]
                 loss_policy_v = -log_prob_actions_v.mean()
 
-                prob_v = F.softmax(logits_v)
+                prob_v = F.softmax(logits_v, dim=1)
                 entropy_loss_v = ENTROPY_BETA * (prob_v * log_prob_v).sum(dim=1).mean()
 
                 loss_v = loss_policy_v + entropy_loss_v + loss_value_v