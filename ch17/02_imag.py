Softmax fixes

ch17/02_imag.py
@@ -45,7 +45,7 @@ def iterate_batches(envs, net, cuda=False):
     while True:
         obs_v = ptan.agent.default_states_preprocessor(obs, cuda=cuda)
         logits_v, values_v = net(obs_v)
-        probs_v = F.softmax(logits_v)
+        probs_v = F.softmax(logits_v, dim=1)
         probs = probs_v.data.cpu().numpy()
         actions = act_selector(probs)
 

ch17/lib/common.py
@@ -110,7 +110,7 @@ def iterate_batches(envs, net, cuda=False):
             if cuda:
                 obs_v = obs_v.cuda()
             logits_v, values_v = net(obs_v)
-            probs_v = F.softmax(logits_v)
+            probs_v = F.softmax(logits_v, dim=1)
             probs = probs_v.data.cpu().numpy()
             actions = act_selector(probs)
             mb_probs[:, n] = probs
@@ -164,13 +164,13 @@ def train_a2c(net, mb_obs, mb_rewards, mb_actions, mb_values, optimizer, tb_trac
         rewards_v = rewards_v.cuda()
         actions_t = actions_t.cuda()
     logits_v, values_v = net(obs_v)
-    log_prob_v = F.log_softmax(logits_v)
+    log_prob_v = F.log_softmax(logits_v, dim=1)
     log_prob_actions_v = adv_v * log_prob_v[range(len(mb_actions)), actions_t]
 
     loss_policy_v = -log_prob_actions_v.mean()
     loss_value_v = F.mse_loss(values_v, rewards_v)
 
-    prob_v = F.softmax(logits_v)
+    prob_v = F.softmax(logits_v, dim=1)
     entropy_loss_v = (prob_v * log_prob_v).sum(dim=1).mean()
     loss_v = ENTROPY_BETA * entropy_loss_v + VALUE_LOSS_COEF * loss_value_v + loss_policy_v
     loss_v.backward()

ch17/lib/i2a.py
@@ -181,7 +181,7 @@ class I2A(nn.Module):
             obs_batch_v = torch.cat((cur_plane_v, new_plane_v), dim=1)
             # select actions
             logits_v, _ = self.net_policy(obs_batch_v)
-            probs_v = F.softmax(logits_v)
+            probs_v = F.softmax(logits_v, dim=1)
             probs = probs_v.data.cpu().numpy()
             actions = self.action_selector(probs)
         step_obs_v = torch.stack(step_obs)