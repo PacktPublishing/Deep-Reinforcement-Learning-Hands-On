9e80c11073af48db2876fc943df9264a7ab0488e
Max Lapan <max.lapan@gmail.com>
2018-04-29 10:24:51 +0200

    0.4.0 update

--- ch11/02_a3c_grad.py
+++ ch11/02_a3c_grad.py
@@ -9,7 +9,6 @@ import torch.nn.utils as nn_utils
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.multiprocessing as mp
-from torch.autograd import Variable
 
 from lib import common
 
@@ -42,10 +41,10 @@ def make_env():
     return ptan.common.wrappers.wrap_dqn(gym.make(ENV_NAME))
 
 
-def grads_func(proc_name, net, cuda, train_queue):
+def grads_func(proc_name, net, device, train_queue):
     envs = [make_env() for _ in range(NUM_ENVS)]
 
-    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], cuda=cuda, apply_softmax=True)
+    agent = ptan.agent.PolicyAgent(lambda x: net(x)[0], device=device, apply_softmax=True)
     exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, gamma=GAMMA, steps_count=REWARD_STEPS)
 
     batch = []
@@ -65,12 +64,12 @@ def grads_func(proc_name, net, cuda, train_queue):
                     continue
 
                 states_v, actions_t, vals_ref_v = \
-                    common.unpack_batch(batch, net, last_val_gamma=GAMMA**REWARD_STEPS, cuda=cuda)
+                    common.unpack_batch(batch, net, last_val_gamma=GAMMA**REWARD_STEPS, device=device)
                 batch.clear()
 
                 net.zero_grad()
                 logits_v, value_v = net(states_v)
-                loss_value_v = F.mse_loss(value_v, vals_ref_v)
+                loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
 
                 log_prob_v = F.log_softmax(logits_v, dim=1)
                 adv_v = vals_ref_v - value_v.detach()
@@ -92,7 +91,7 @@ def grads_func(proc_name, net, cuda, train_queue):
                 tb_tracker.track("loss_total", loss_v, frame_idx)
 
                 # gather gradients
-                nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+                nn_utils.clip_grad_norm_(net.parameters(), CLIP_GRAD)
                 grads = [param.grad.data.cpu().numpy() if param.grad is not None else None
                          for param in net.parameters()]
                 train_queue.put(grads)
@@ -106,11 +105,10 @@ if __name__ == "__main__":
     parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
     parser.add_argument("-n", "--name", required=True, help="Name of the run")
     args = parser.parse_args()
+    device = "cuda" if args.cuda else "cpu"
 
     env = make_env()
-    net = common.AtariA2C(env.observation_space.shape, env.action_space.n)
-    if args.cuda:
-        net.cuda()
+    net = common.AtariA2C(env.observation_space.shape, env.action_space.n).to(device)
     net.share_memory()
 
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE, eps=1e-3)
@@ -119,7 +117,7 @@ if __name__ == "__main__":
     data_proc_list = []
     for proc_idx in range(PROCESSES_COUNT):
         proc_name = "-a3c-grad_" + NAME + "_" + args.name + "#%d" % proc_idx
-        data_proc = mp.Process(target=grads_func, args=(proc_name, net, args.cuda, train_queue))
+        data_proc = mp.Process(target=grads_func, args=(proc_name, net, device, train_queue))
         data_proc.start()
         data_proc_list.append(data_proc)
 
@@ -143,12 +141,9 @@ if __name__ == "__main__":
 
             if step_idx % TRAIN_BATCH == 0:
                 for param, grad in zip(net.parameters(), grad_buffer):
-                    grad_v = Variable(torch.from_numpy(grad))
-                    if args.cuda:
-                        grad_v = grad_v.cuda()
-                    param.grad = grad_v
+                    param.grad = torch.FloatTensor(grad).to(device)
 
-                nn_utils.clip_grad_norm(net.parameters(), CLIP_GRAD)
+                nn_utils.clip_grad_norm_(net.parameters(), CLIP_GRAD)
                 optimizer.step()
                 grad_buffer = None
     finally:
