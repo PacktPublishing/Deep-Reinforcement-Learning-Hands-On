6871e607e87d38ebf95feb01616db450c23c9c19
Max Lapan <max.lapan@gmail.com>
2018-05-02 09:55:14 +0200

    Fix wrong device

--- ch18/train.py
+++ ch18/train.py
@@ -38,7 +38,7 @@ def evaluate(net1, net2, rounds, device="cpu"):
     for r_idx in range(rounds):
         r, _ = model.play_game(mcts_stores=mcts_stores, replay_buffer=None, net1=net1, net2=net2,
                                steps_before_tau_0=0, mcts_searches=20, mcts_batch_size=16,
-                               device="cpu")
+                               device=device)
         if r < -0.5:
             n2_win += 1
         elif r > 0.5:
