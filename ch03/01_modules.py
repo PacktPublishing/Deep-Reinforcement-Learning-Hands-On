a48fa95d7c0cb4e27fadff1c55bda3dec1e1d13c
Omrigan <omrigann@gmail.com>
2018-05-02 17:40:45 +0300

    Added cuda transfer check

--- ch03/01_modules.py
+++ ch03/01_modules.py
@@ -24,3 +24,5 @@ if __name__ == "__main__":
     out = net(v)
     print(out)
     print("Cuda's availability is %s" % torch.cuda.is_available())
+    if torch.cuda.is_available():
+        print("Data from cuda: %s" % out.to('cuda'))
