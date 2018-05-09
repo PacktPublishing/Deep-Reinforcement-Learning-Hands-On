cbd9d9ccd77c7c3bde3d66dc985a36761eac701f
Max Lapan <max.lapan@gmail.com>
2018-05-09 14:36:38 +0200

    Use patched image id

--- ch13/adhoc/start_docker_demo.sh
+++ ch13/adhoc/start_docker_demo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -x
-docker run -e TURK_DB='' -p 5899:5899 --privileged --ipc host --cap-add SYS_ADMIN 7499f41eed5e demonstration -e ${1:-wob.mini.ClickTest2-v0}
+docker run -e TURK_DB='' -p 5899:5899 --privileged --ipc host --cap-add SYS_ADMIN shmuma/miniwob demonstration -e ${1:-wob.mini.ClickTest2-v0}
